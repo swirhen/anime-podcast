0859825b954320aa71fc0c4ed97efcd8808de4b1
swirhen <swirhen@hmx-51seto.(none)>
2010-02-24 18:39:03 +0900

    mumuu

---
+++ tdlstop.sh
@@ -0,0 +1,35 @@
+date=`date '+%y/%m/%d %H:%M:%S'`
+sleep 100
+n=`ls *.aria2 2>/dev/null | wc -l`
+if [ $n -gt 1 ]; then
+  for a in *.aria2
+  do
+    filea=`echo $a | cut -d"." -f1,2`
+    file="$filea(ほか`expr $n - 1`個)"
+    break
+  done
+elif [ $n -eq 1 ]; then
+  filea=`ls *.aria2`
+  file=`echo $filea | cut -d"." -f1,2`
+fi
+echo "$fileの栽培監視を開始"
+while true
+do
+  pid=`ps -ef | grep port=$1 | grep -v grep | awk '{print $2}'`
+  if [ "${pid:-null}" = null ]; then
+    echo "\n### watching aria2c(port=$1) end - pid lost###"
+    exit
+  else
+    if [ `ls *.aria2 2>/dev/null | wc -l` -gt 0  ]; then
+      echo "\n### watching aria2c(port=$1) ###"
+      sleep 5
+    else
+      kill $pid
+      sleep 2
+      kill $pid
+      echo "\n### watching aria2c(port=$1) end###"
+      /data/share/movie/tw.py "@swirhen 栽培完了:$date 開始のファイル: $file"
+      exit
+    fi
+  fi
+done
