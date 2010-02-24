199d937c432c74b6c6f2a41235e9a93cb0b4f3bd
swirhen <swirhen@hmx-51seto.(none)>
2010-02-24 19:02:09 +0900

    アニメリスト

---
+++ mkvremarge.sh
@@ -0,0 +1,11 @@
+NAME=`echo $1 | cut -d"." -f1`
+echo $NAME
+mkvextract tracks "$1" 1:video.h264
+mkvextract tracks "$1" 2:audio.aac
+#/usr/local/bin/ffmpeg -i audio.ac3 -acodec libfaac -ar 48000 -ab 128k audio.aac
+MP4Box -fps 23.976025 -add video.h264 -add audio.aac -new "$NAME.mp4"
+#MP4Box -fps 29.970030 -add video.h264:par=10:11 -add audio.aac -new "$NAME.mp4"
+#MP4Box -fps 29.970030 -add video.h264 -add audio.aac -new "$NAME.mp4"
+#MP4Box -fps 30.00 -add video.h264 -add audio.aac -new "$NAME.mp4"
+rm video.h264
+rm audio.*
