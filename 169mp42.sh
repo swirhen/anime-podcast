8a84405b1ad0caba3e924a12083ddbfbec1c7509
swirhen <swirhen@hmx-51seto.(none)>
2010-02-26 04:00:13 +0900

    mata

--- 169mp42.sh
+++ 169mp42.sh
@@ -1,15 +1,15 @@
 # /bin/sh
 # @author swirhen
-# ffmpeg$B$r$D$+$C$F(BPSP$B$d$"$$$[$s$`$1$N$I$&$,$r$D$/$k(B
-# wine$B$r$D$+$C$F(Bwindows$B$P$s$N$P$$$J$j$r$D$+$C$F$7$^$&HG(B
+# ffmpegをつかってPSPやあいほんむけのどうがをつくる
+# wineをつかってwindowsばんのばいなりをつかってしまう版
 # usage:169mp42.sh [infile] [out directory] [encode option]
-# $B$I$i$$$V$,$A$,$&$I$&$,$N$H$-$K(Btmp$B$r$F$-$;$D$J$P$7$g$K$+$($k(B
+# どらいぶがちがうどうがのときにtmpをてきせつなばしょにかえる
 tmp=`readlink -f "$PWD"`
 drive=`expr "$tmp" : "\/\(data.\?\)\/.*"` if [ ${drive:-null} = null ] ; then
   drive=data
 fi
-# $B$$$m$$$m$A$'$C$/(B
-# $B$X$s$J(Bfps$B$N$I$&$,$N$P$"$$(B 30000/1001$B$K$H$&$$$D$7$F$4$^$+$9(B
+# いろいろちぇっく
+# へんなfpsのどうがのばあい 30000/1001にとういつしてごまかす
 fpsfix="-r 30000/1001"
 /usr/bin/ffmpeg -i "$1" 2>> /tmp/fps.txt
 echo "$1 fps check"
@@ -20,8 +20,8 @@ if [ $fpsck -eq 0 ]; then
   echo "# $1 is invalid fps!"
   opt=$fpsfix
 fi
-# $B$"$9$Z$/$H$R$r$8$I$&$O$s$Y$D$9$k(B
-# 4:3$B$@$C$?$i(B360x270$B$K$9$k$1$I!"(B1440x1080$B$@$1$O$d$i$J$$(B
+# あすぺくとひをじどうはんべつする
+# 4:3だったら360x270にするけど、1440x1080だけはやらない
 echo "$1 aspect check"
 echo `egrep '[0-9][0-9][0-9]+x[0-9][0-9][0-9]+,' /tmp/fps.txt`
 asck=`egrep '[0-9][0-9][0-9]+x[0-9][0-9][0-9]+,' /tmp/fps.txt`
@@ -38,8 +38,8 @@ if [ $asw = "1440" -a $ash = "1080" ]; then
 	wide="480"
 fi
 rm /tmp/fps.txt
-# $B$($s$3$9$k(B
+# えんこする
 /usr/bin/wine ffmpeg.exe -i "$1" -s "$wide"x270 -vcodec libx264 -b 500k -acodec libfaac -ac 2 -ar 48000 -ab 128k -async 100 -crf 20 -g 230 -mbd 2 -me_method umh -subq 6 -qdiff 6 -me_range 32 -nr 50 -qmin 12 -sc_threshold 65 -bidir_refine 1 -keyint_min 3 -cmp chroma -flags bitexact+alt+mv4+loop -f mp4 -coder 0 -level 13 -threads 0 $3 $opt /$drive/tmp/"$1".mp4
-# MP4Box$B$G(Bfaststart$B$?$$$*$&$K$9$k(B
+# MP4Boxでfaststartたいおうにする
 /usr/local/bin/MP4Box -ipod /$drive/tmp/"$1".mp4
 /bin/mv -v /$drive/tmp/"$1".mp4 "$2""$1".mp4
