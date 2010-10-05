1c2117c4130e8f07092a4727a1f4860fce86260f
swirhen <swirhen@hmx-51seto.(none)>
2010-10-05 15:58:07 +0900

    ひさしぶり

--- 169f.sh
+++ 169f.sh
@@ -7,9 +7,11 @@ do
     /data/share/movie/sh/169mp42.sh "$a" "/data/share/movie/98 PSP用/"
     /data/share/movie/sh/mmpc.sh
     /home/swirhen/sh/ftpmount.sh
-    cp -v "/data/share/movie/98 PSP用/$a.mp4" "/data/share/movie/97 bashi.org/"
-    /data/share/movie/sh/mbmpc.sh
+#    cp -v "/data/share/movie/98 PSP用/$a.mp4" "/data/share/movie/97 bashi.org/"
+#    /data/share/movie/sh/mbmpc.sh
+    cp -v "/data/share/movie/98 PSP用/$a.mp4" "/data/share/movie/96 swirhen.net/"
+    /data/share/movie/sh/msmpc.sh
     /data/share/movie/sh/mmv.sh "$a"
-    /data/share/movie/tw.py "【publish】$a.mp4"
+    /home/swirhen/Shellscriptter/Shellscriptter.sh -r "【publish】$a.mp4"
   fi
 done
