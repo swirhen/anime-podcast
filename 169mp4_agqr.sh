4e5fbd294c46f113bc3b665e8cafbe4856f19af3
swirhen <swirhen@gmail.com>
2012-07-31 21:47:19 +0900

    説明書きを変更した

--- 169mp4_agqr.sh
+++ 169mp4_agqr.sh
@@ -2,10 +2,8 @@
 # @author swirhen
 # ffmpegをつかってPSPやあいほんむけのどうがをつくる
 # wineをつかってwindowsばんのばいなりをつかってしまう版
-# あいほん4やNGPなどにたいおうするさくていをほどこした こうかいぞうど版
-# usage:169mp43.sh [infile] [out directory] [encode option] [encode option(before)] [force aspect ratio]
-# 4つめのひきすうはサイズけっていのまえにえんこーどおぷしょんをつけたいとき
-# 5こめのひきすうは"0"だときょうせいてきに4:3 "1"なら16:9となる
+# ぶんかほうそう ちょーA&Gせんよう てきとうえんこばーじょん
+# usage:169mp43.sh [infile] [out directory]
 
 # どらいぶがちがうどうがのときにtmpをてきせつなばしょにかえる
 # じっこうでぃれくとりから ドライブをはんだん
@@ -22,8 +20,7 @@ if [ ${drive:-null} = null ] ; then
 fi
 # えんこする
 /usr/bin/wine ffmpeg.exe -i "$1" -vcodec copy -f mp4 -acodec libfaac -ac 2 -ar 24000 -ab 64k -threads 0 $3 /$drive/tmp/"$1".mp4
-#/usr/bin/wine ffmpeg.exe -i "$1" $4 -s "320x240" -b 500k -vcodec libx264 -trellis 2 -bf 3 -b_strategy 1 -bidir_refine 1 -crf 25 -g 240 -mbd 2 -me_method umh -subq 6 -qdiff 6 -me_range 32 -sc_threshold 65 -keyint_min 3 -nr 100 -qmin 12 -sn -flags bitexact+alt+mv4+loop -flags2 mixed_refs -partitions parti4x4+partp4x4+partp8x8 -f mp4 -coder 1 -level 30 -acodec libfaac -ac 2 -ar 48000 -ab 96k -async 100 -threads 0 $3 /$drive/tmp/"$1".mp4
-# MP4Boxで faststartたいおうにする アスペクトひをしていする
+# MP4Boxで faststartたいおうにする
 sleep 5
 /usr/local/bin/MP4Box -ipod -par 1=1:1 /$drive/tmp/"$1".mp4 -out /$drive/tmp/"$1"_mod.mp4
 rm /$drive/tmp/"$1".mp4
