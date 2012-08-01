baefd4f1a294309524394b4869d30d009d1d3dc4
swirhen <swirhen@gmail.com>
2012-08-01 09:47:33 +0900

    ひきすうかえた

--- agqrrecord.sh
+++ agqrrecord.sh
@@ -1,18 +1,24 @@
 # /bin/sh
-# 超A&G予約録画シェルスクリプト
+# 超A&G予約録画用スクリプト
 # require: rtmpdump,ffmpeg,いろいろ
-# usage: agqrrecord.sh [番組名] [録画時間] [動画フラグ] [隔週フラグファイル名]
+# usage: agqrrecord.sh [番組名] [開始オフセット] [録画時間] [動画フラグ] [隔週フラグファイル名]
 # 番組名: YYYYMMDD_HHMM_番組名.flv というファイルになる
+* 開始オフセット: sec
 # 録画時間: sec
 # 動画フラグ: vなら映像付き、それ以外なら音声と見なしてエンコする
 # 隔週フラグファイル名: 
 #     フラグファイルがあるかどうかチェックして、なければ作成だけして録画しない
 #     あれば削除して録画する
+name=$1
+offset=$2
+rectime=$3
+vidflg=$4
+recflg=$5
 # オフセット
-sleep 45
+sleep $offset
 # 隔週対応
-if [ "${4:-null}" != null ]; then
-flgfile="/data/share/movie/98 PSP用/agqr/flg/$4"
+if [ "${recflg:-null}" != null ]; then
+flgfile="/data/share/movie/98 PSP用/agqr/flg/$recflg"
 	if [ -f "$flgfile" ]; then
 		rm "$flgfile"
 	else
@@ -23,9 +29,9 @@ fi
 # 日付時刻
 dt=`date +"%Y%m%d_%H%M"`
 # 保存ファイル名
-filename="/data/share/movie/98 PSP用/agqr/flv/""$dt"_"$1.flv"
+filename="/data/share/movie/98 PSP用/agqr/flv/""$dt"_"$name.flv"
 # パスを除いたファイル名
-efilename="$dt"_"$1.flv"
+efilename="$dt"_"$name.flv"
 # 接続失敗対策、ファイルが生成されるまで処理を繰り返す
 until [ -s "$filename" ]
 do
@@ -34,12 +40,12 @@ do
 	num2=`expr $RANDOM % 2 + 1`
 	num3=`expr $RANDOM % 2 + 1`
 	# echo "/usr/local/bin/rtmpdump --rtmp "rtmpe://fms${num}.uniqueradio.jp/" --playpath "aandg${num2}" --app "?rtmp://fms-base${num3}.mitene.ad.jp/agqr/" --live -o "$filename" --stop $2"
-	/usr/local/bin/rtmpdump --rtmp "rtmpe://fms${num}.uniqueradio.jp/" --playpath "aandg${num2}" --app "?rtmp://fms-base${num3}.mitene.ad.jp/agqr/" --live -o "$filename" --stop $2
+	/usr/local/bin/rtmpdump --rtmp "rtmpe://fms${num}.uniqueradio.jp/" --playpath "aandg${num2}" --app "?rtmp://fms-base${num3}.mitene.ad.jp/agqr/" --live -o "$filename" --stop $rectime
 done
 # 保存フォルダへ移動
 cd "/data/share/movie/98 PSP用/agqr/flv"
 # 映像付きならばエンコード用のシェルを呼ぶ。音声のみならmp3エンコード
-if [ $3 = v ]; then
+if [ $vidflg = v ]; then
 	/data/share/movie/sh/169mp4_agqr.sh "$efilename" "/data/share/movie/98 PSP用/agqr/"
 else 
 	/usr/bin/wine ffmpeg.exe -i "$efilename" -acodec libmp3lame -ab 64k -ac 2 -ar 24000 "/data/share/movie/98 PSP用/agqr/$efilename.mp3"
