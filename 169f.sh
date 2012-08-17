c51871b226bf47ad675e8a9dca46cf45cdd78941
swirhen <swirhen@gmail.com>
2012-08-17 10:03:40 +0900

    ffmpegが途中でこけたときの処理と、優先して処理するファイルを指定できるようにした

--- 169f.sh
+++ 169f.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/env zsh
 # 動画エンコード用シェル
 source /home/swirhen/.zshrc
-for a in "$1"*話*.(avi|mp4|mkv|wmv)
+for a in "$@" *話*.(avi|mp4|mkv|wmv)
 do
   if [ -f "$a" ]; then
-    /data/share/movie/sh/169mp43.sh "$a" "/data/share/movie/98 PSP用/"
+    until [ -f "/data/share/movie/98 PSP用/$a.mp4" ];
+    do
+      /data/share/movie/sh/169mp43.sh "$a" "/data/share/movie/98 PSP用/"
+    done
     /data/share/movie/sh/mmpc.sh
-#    /data/share/movie/sh/mmpc2.sh
     /data/share/movie/sh/mmpc3.sh
-#    /home/swirhen/sh/ftpmount.sh
-#    cp -v "/data/share/movie/98 PSP用/$a.mp4" "/data/share/movie/97 bashi.org/"
-#    /data/share/movie/sh/mbmpc.sh
-#    cp -v "/data/share/movie/98 PSP用/$a.mp4" "/data/share/movie/96 swirhen.net/"
-#    /data/share/movie/sh/msmpc.sh
     /data/share/movie/sh/mmv.sh "$a"
     sleep 5
     /home/swirhen/Shellscriptter/Shellscriptter.sh -r "【publish】$a.mp4"
