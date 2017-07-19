a7f3328a57fb0a1fae8e2e3613f7051798d059de
swirhen <irankae1#>
2017-07-19 17:50:20 +0900

    リネームシェルの改造 スペース含むヒット句に対応(テキストの区切り句を|とする) リストをchecklist.txtに統一

--- mmv.sh
+++ mmv.sh
@@ -2,10 +2,10 @@
 # usage: ./rename.sh 与えるファイル群 リネーム用リスト
 # リネーム用リストにはリネームもとファイルの検索文字と正式な作品名を
 # tabで繋げて記述すること
-LIST=/data/share/movie/name.lst
-LIST2=/data/share/movie/name2.lst
-while read SF NAME
+LIST=/data/share/movie/checklist.txt
+while read DUMMY DUMMY LINE
 do
+  NAME="${LINE#*\|}"
   nn=`echo $1 | sed -e "s/\(${NAME}\).*\.mp4/\1/"`
   if [ "$nn" != "$1" ]; then
     echo "# move $1"
@@ -13,12 +13,3 @@ do
     break
   fi
 done < ${LIST}
-while read SF NAME
-do
-  nn=`echo $1 | sed -e "s/\(${NAME}\).*\.mp4/\1/"`
-  if [ "$nn" != "$1" ]; then
-    echo "# move $1"
-    mv -v "$1" /data/share/movie/*"${NAME}"/.
-    break
-  fi
-done < ${LIST2}
