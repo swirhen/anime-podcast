e1bae20901e56e90b611bb73853ed6e9f4219362
swirhen <irankae1#>
2017-09-22 11:48:25 +0900

    youtubedlのしゅうせい

--- youtubedl.sh
+++ youtubedl.sh
@@ -8,8 +8,16 @@ mkdir /data/tmp/${DATETIME}
 cd /data/tmp/${DATETIME}
 python /data/share/movie/sh/youtube-dl "$1"
 if [ -f *.f*.webm ]; then
-  name=`ls *.*.mp4`
-  ffm3 -i *.*.mp4 -i *.*.webm -vcodec copy -acodec aac ${nowdir}/"${name}"
+  if [ `ls *.f*.webm | wc -l` -eq 2 ]; then
+    name=`ls *.f*.webm | head -1`
+    name2=`ls *.f*.webm | tail -1`
+    ffm3 -i "${name}" -i "${name2}" -vcodec copy -acodec aac ${nowdir}/"${name}"
+  else
+    name=`ls *.*.mp4`
+    ffm3 -i *.*.mp4 -i *.*.webm -vcodec copy -acodec aac ${nowdir}/"${name}"
+  fi
 else
   mv *.*.mp4 ${nowdir}
 fi
+cd ${nowdir}
+rm -rf /data/tmp/${DATETIME}
