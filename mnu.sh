940ab53862c92e4346bff4bbd2f8532be646cb0f
swirhen <swirhen@gmail.com>
2018-01-15 13:46:52 +0900

    debug

--- mnu.sh
+++ mnu.sh
@@ -7,17 +7,21 @@
 cnt=1
 for dir in *
 do
-    if [[ "${dir}" !=~ ^9[2-9]\|^00\|^sh && -d "${dir}" ]]; then
-        NAME=`echo "${dir}" | sed "s/^[0-9][0-9]\ \(.*\)/\1/"`
-        #NUM=`echo "${dir}" | sed "s/^\([0-9][0-9]\)\ .*/\1/"`
-        cnt=`printf %02d ${cnt}`
+    if [[ "${dir}" =~ ^9[2-9]\|^00\|^sh ]]; then
+        echo "${dir} : 処理除外"
+    else
+        if [ -d = "${dir}" ]; then
+            NAME=`echo "${dir}" | sed "s/^[0-9][0-9]\ \(.*\)/\1/"`
+            #NUM=`echo "${dir}" | sed "s/^\([0-9][0-9]\)\ .*/\1/"`
+            cnt=`printf %02d ${cnt}`
 
-        if [ $# -ne 0 -a "$1" = "-r" -a "${dir}" != "${NAME}" ]; then
-            echo "# rename ${dir} -> ${NAME}"
-            mv "${dir}" "${NAME}"
-        elif [ "${dir}" != "${cnt} ${NAME}" ]; then
-            echo "# rename ${dir} -> ${cnt} ${NAME}"
-            mv "${dir}" "${cnt} ${NAME}"
+            if [ $# -ne 0 -a "$1" = "-r" -a "${dir}" != "${NAME}" ]; then
+                echo "# rename ${dir} -> ${NAME}"
+                mv "${dir}" "${NAME}"
+            elif [ "${dir}" != "${cnt} ${NAME}" ]; then
+                echo "# rename ${dir} -> ${cnt} ${NAME}"
+                mv "${dir}" "${cnt} ${NAME}"
+            fi
         fi
     fi
     (( cnt++ ))
