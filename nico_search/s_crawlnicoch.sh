bf3fb988723cf31a97fc18b1a9ff8e5494978f3d
swirhen <swirhen@gmail.com>
2018-01-31 18:30:24 +0900

    modify: ニコニコダウンロードのシェルを指定できるようにする

--- nico_search/s_crawlnicoch.sh
+++ nico_search/s_crawlnicoch.sh
@@ -3,6 +3,7 @@
 # 引数:
 # 1: 検索ワード
 # 2: なんでもいいので入れると音声モード(コマンドの後ろに aがつく)
+NICODL_CMD="/data/share/movie/sh/nicodl.sh"
 SRC_WORD="$1"
 SRC_WORD_ENC=`echo "${SRC_WORD}" | nkf -wMQ | sed 's/=$//g' | tr = % | tr -d "\n"`
 AUDIO_FLG=""
@@ -13,7 +14,7 @@ fi
 PAGE=1
 while :
 do
-    result=`curl -sS "http://www.nicovideo.jp/search/${SRC_WORD_ENC}?sort=f&order=d&page=${PAGE}" | grep ".*a title.*${SRC_WORD}" | sed "s#^.*<a.*title=\"\(.*\)\".*href=\"\(.*\)?ref.*#/data/share/movie/sh/nicodl.sh \"http://www.nicovideo.jp\2\" \"\1\"#" | sed "s/\ href.*/ ${AUDIO_FLG}/"`
+    result=`curl -sS "http://www.nicovideo.jp/search/${SRC_WORD_ENC}?sort=f&order=d&page=${PAGE}" | grep ".*a title.*${SRC_WORD}" | sed "s#^.*<a.*title=\"\(.*\)\".*href=\"\(.*\)?ref.*#${NICODL_CMD} \"http://www.nicovideo.jp\2\" \"\1\"#" | sed "s/\ href.*/ ${AUDIO_FLG}/"`
     if [ "${result}" != "" ]; then
         echo ${result}
     else
