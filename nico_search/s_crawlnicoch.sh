b19bb9c4cd1df7b2cb98c0ed8ecf630aed9dc829
swirhen <swirhen@gmail.com>
2018-01-31 18:25:46 +0900

    modify: ページクロール

--- nico_search/s_crawlnicoch.sh
+++ nico_search/s_crawlnicoch.sh
@@ -5,14 +5,19 @@
 # 2: なんでもいいので入れると音声モード(コマンドの後ろに aがつく)
 SRC_WORD="$1"
 SRC_WORD_ENC=`echo "${SRC_WORD}" | nkf -wMQ | sed 's/=$//g' | tr = % | tr -d "\n"`
-echo "hogeee:${SRC_WORD_ENC}"
 AUDIO_FLG=""
-PAGE=1
 if [ "$2" != "" ]; then
 	AUDIO_FLG=" a"
 fi
-if [ "$3" != "" ]; then
-	PAGE=$3
-fi
-
-curl "http://www.nicovideo.jp/search/${SRC_WORD_ENC}?sort=f&order=d&page=${PAGE}" | grep ".*a title.*${SRC_WORD}" | sed "s#^.*<a.*title=\"\(.*\)\".*href=\"\(.*\)?ref.*#/data/share/movie/sh/nicodl.sh \"http://www.nicovideo.jp\2\" \"\1\"#" | sed "s/\ href.*/ ${AUDIO_FLG}/"
+# 複数ページあったら結果がなくなるまでクロールする
+PAGE=1
+while :
+do
+    result=`curl "http://www.nicovideo.jp/search/${SRC_WORD_ENC}?sort=f&order=d&page=${PAGE}" | grep ".*a title.*${SRC_WORD}" | sed "s#^.*<a.*title=\"\(.*\)\".*href=\"\(.*\)?ref.*#/data/share/movie/sh/nicodl.sh \"http://www.nicovideo.jp\2\" \"\1\"#" | sed "s/\ href.*/ ${AUDIO_FLG}/"`
+    if [ "${result}" != "" ]; then
+        echo ${result}
+    else
+        break
+    fi
+    (( PAGE++ ))
+done
