db43d8ba0d0bc8b5b4534360124cf99bdf79086d
swirhen <swirhen@gmail.com>
2018-02-01 18:09:51 +0900

    add: ニコニコ系総合クローラー デバッグ中

--- nico_search/auto_crawl.sh
+++ nico_search/auto_crawl.sh
@@ -89,13 +89,21 @@ SAVE_DIR_NUM="${SAVE_DIR_NUMS[${cnt}]}"
 NUM_PREFIX=${NUM_PREFIXS[${cnt}]}
 NUM_SUFFIX=${NUM_SUFFIXS[${cnt}]}
 SED_STR="${SED_STRS[${cnt}]}"
+if [ "${NUM_PREFIX}" != "" ]; then
+    EPNUM=${NUM_PREFIX}${EP_NUM}
+else
+    EPNUM=${EP_NUM}
+fi
+if [ "${NUM_SUFFIX}" != "" ]; then
+    EPNUM=${EPNUM}${NUM_SUFFIX}
+fi
 
     # curlでURLからクロールする
     if [ "${URL:8:2}" = "ww" ]; then
         result=`curl -sS "${URL}" | grep ".*a title.*${KEYWORD}" | sed "s#^.*<a.*title=\"\(.*\)\".*href=\"\(.*\)?ref.*#${NICODL_CMD} \"http://www.nicovideo.jp\2\" \"\1\"#" | grep ${NUM_PREFIX}${EP_NUM}${NUM_SUFFIX} | sed "${SED_STR}"`
     else
 #        echo "curl -sS \"${URL}\" | grep \"http.*title.*${KEYWORD}\" | sed \"s#^.*<a href=#${NICODL_CMD} #\" | sed \"s/title=//\" | grep ${NUM_PREFIX}${EP_NUM}${NUM_SUFFIX} | sed \"${SED_STR}\""
-        ./crawlnicoch.sh "${URL}" "${KEYWORD}" | grep ${EP_NUM}${NUM_SUFFIX} | sed "${SED_STR}"
+        ./crawlnicoch.sh "${URL}" "${KEYWORD}" | grep ${EPNUM} | sed "${SED_STR}"
     fi
 
 #    if [ "${result}" != "" ]; then
