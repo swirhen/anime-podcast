c6e20f7b9ae1f115e641e93a6b450b240b8a1a41
swirhen <swirhen@gmail.com>
2018-02-01 17:59:48 +0900

    add: ニコニコ系総合クローラー デバッグ中

--- nico_search/auto_crawl.sh
+++ nico_search/auto_crawl.sh
@@ -95,7 +95,7 @@ SED_STR="${SED_STRS[${cnt}]}"
         result=`curl -sS "${URL}" | grep ".*a title.*${KEYWORD}" | sed "s#^.*<a.*title=\"\(.*\)\".*href=\"\(.*\)?ref.*#${NICODL_CMD} \"http://www.nicovideo.jp\2\" \"\1\"#" | grep ${NUM_PREFIX}${EP_NUM}${NUM_SUFFIX} | sed "${SED_STR}"`
     else
 #        echo "curl -sS \"${URL}\" | grep \"http.*title.*${KEYWORD}\" | sed \"s#^.*<a href=#${NICODL_CMD} #\" | sed \"s/title=//\" | grep ${NUM_PREFIX}${EP_NUM}${NUM_SUFFIX} | sed \"${SED_STR}\""
-        ./crawlnicoch.sh "${URL}" "${KEYWORD}" | grep ${NUM_PREFIX}${EP_NUM}${NUM_SUFFIX} | sed "${SED_STR}"
+        ./crawlnicoch.sh "${URL}" "${KEYWORD}" | grep ${NUM_PREFIX}${EP_NUM}${NUM_SUFFIX} #| sed "${SED_STR}"
     fi
 
 #    if [ "${result}" != "" ]; then
