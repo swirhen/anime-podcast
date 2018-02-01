e6b9a502491ea83a26580c13aa796bf8e5c8b650
swirhen <swirhen@gmail.com>
2018-02-01 18:28:36 +0900

    add: ニコニコ系総合クローラー デバッグ中

--- nico_search/auto_crawl.sh
+++ nico_search/auto_crawl.sh
@@ -3,7 +3,7 @@
 # リストから検索URL(チャンネルの場合はビデオリスト、検索の場合は検索URL)をクロールして、キーワードと話数指定で最新話数があったら
 # ダウンロードして保存、ニコニコのフィードにリリースする
 # リスト形式：
-# 最終更新 最新回数 検索URL 検索キーワード 保存ディレクトリ連番 回数プレフィクス 回数サフィクス sed文言
+# 最終更新 最新回数+1 検索URL 検索キーワード 保存ディレクトリ連番 回数プレフィクス 回数サフィクス sed文言
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)"
 LIST_FILE=${SCRIPT_DIR}/checklist.txt
 LIST_TEMP=${SCRIPT_DIR}/checklist.temp
@@ -15,6 +15,7 @@ NICODL_CMD="/data/share/movie/sh/nicodl.sh"
 POST_FLG=1
 LOG_FILE=${SCRIPT_DIR}/autocrawl_${DATETIME2}.log
 FLG_FILE=${SCRIPT_DIR}/autocrawl_running
+DL_SH=${SCRIPT_DIR}/dl.sh
 
 end() {
     mv ${LOG_FILE} ${SCRIPT_DIR}/logs/
@@ -68,51 +69,57 @@ do
 done < ${LIST_FILE}
 
 cnt=0
+dl_flg=0
 while :
 do
     if [ "${LAST_UPDS[${cnt}]}" = "" ]; then
         break
     fi
 #    echo "LAST_UPDS: ${LAST_UPDS[${cnt}]}"
-    echo "EP_NUMS: ${EP_NUMS[${cnt}]}"
-    echo "URLS: ${URLS[${cnt}]}"
-    echo "KEYWORDS: ${KEYWORDS[${cnt}]}"
+#    echo "EP_NUMS: ${EP_NUMS[${cnt}]}"
+#    echo "URLS: ${URLS[${cnt}]}"
+#    echo "KEYWORDS: ${KEYWORDS[${cnt}]}"
 #    echo "SAVE_DIR_NUMS: ${SAVE_DIR_NUMS[${cnt}]}"
-    echo "NUM_PREFIXS: ${NUM_PREFIXS[${cnt}]}"
-    echo "NUM_SUFFIXS: ${NUM_SUFFIXS[${cnt}]}"
-    echo "SED_STRS: ${SED_STRS[${cnt}]}"
-LAST_UPD="${LAST_UPDS[${cnt}]}"
-EP_NUM=${EP_NUMS[${cnt}]}
-URL="${URLS[${cnt}]}"
-KEYWORD="${KEYWORDS[${cnt}]}"
-SAVE_DIR_NUM="${SAVE_DIR_NUMS[${cnt}]}"
-NUM_PREFIX=${NUM_PREFIXS[${cnt}]}
-NUM_SUFFIX=${NUM_SUFFIXS[${cnt}]}
-SED_STR="${SED_STRS[${cnt}]}"
-if [ "${NUM_PREFIX}" != "|" ]; then
-    EPNUM=${NUM_PREFIX}${EP_NUM}
-else
-    EPNUM=${EP_NUM}
-fi
-if [ "${NUM_SUFFIX}" != "|" ]; then
-    EPNUM=${EPNUM}${NUM_SUFFIX}
-fi
-echo "epnum: ${EPNUM}"
+#    echo "NUM_PREFIXS: ${NUM_PREFIXS[${cnt}]}"
+#    echo "NUM_SUFFIXS: ${NUM_SUFFIXS[${cnt}]}"
+#    echo "SED_STRS: ${SED_STRS[${cnt}]}"
+    LAST_UPD="${LAST_UPDS[${cnt}]}"
+    EP_NUM=${EP_NUMS[${cnt}]}
+    URL="${URLS[${cnt}]}"
+    KEYWORD="${KEYWORDS[${cnt}]}"
+    SAVE_DIR_NUM="${SAVE_DIR_NUMS[${cnt}]}"
+    NUM_PREFIX=${NUM_PREFIXS[${cnt}]}
+    NUM_SUFFIX=${NUM_SUFFIXS[${cnt}]}
+    SED_STR="${SED_STRS[${cnt}]}"
+    if [ "${NUM_PREFIX}" != "|" ]; then
+        EPNUM=${NUM_PREFIX}${EP_NUM}
+    else
+        EPNUM=${EP_NUM}
+    fi
+    if [ "${NUM_SUFFIX}" != "|" ]; then
+        EPNUM=${EPNUM}${NUM_SUFFIX}
+    fi
 
     # curlでURLからクロールする
     if [ "${URL:8:2}" = "ww" ]; then
-        result=`curl -sS "${URL}" | grep ".*a title.*${KEYWORD}" | sed "s#^.*<a.*title=\"\(.*\)\".*href=\"\(.*\)?ref.*#${NICODL_CMD} \"http://www.nicovideo.jp\2\" \"\1\"#" | grep ${NUM_PREFIX}${EP_NUM}${NUM_SUFFIX} | sed "${SED_STR}"`
+        curl -sS "${URL}" | grep ".*a title.*${KEYWORD}" | sed "s#^.*<a.*title=\"\(.*\)\".*href=\"\(.*\)?ref.*#${NICODL_CMD} \"http://www.nicovideo.jp\2\" \"\1\"#" | grep "${EPNUM}" | sed "${SED_STR}" > ${DL_SH}
     else
-#        echo "curl -sS \"${URL}\" | grep \"http.*title.*${KEYWORD}\" | sed \"s#^.*<a href=#${NICODL_CMD} #\" | sed \"s/title=//\" | grep ${NUM_PREFIX}${EP_NUM}${NUM_SUFFIX} | sed \"${SED_STR}\""
-        ./crawlnicoch.sh "${URL}" "${KEYWORD}" | grep "${EPNUM}" | sed "${SED_STR}"
+        curl -sS "${URL}" | grep "http.*title.*${KEYWORD}" | sed "s#^.*<a href=#${NICODL_CMD} #" | sed "s/title=//" | grep "${EPNUM}" | sed "${SED_STR}" > ${DL_SH}
     fi
 
-#    if [ "${result}" != "" ]; then
-#        echo "${result}"
-#    else
-#        continue
-#    fi
-
+    # dl.shが吐かれたらdl.shを実行
+    # 最新話数をインクリメントして、tempリストに吐き出す(最終更新を更新)
+    # 吐かれていなければインクリメントせずに吐き出す
+    if [ -s ${DL_SH} ]; then
+        dl_flg=1
+        chmod +x ${DL_SH}
+#        ${DL_SH}
+        cat ${DL_SH}
+        (( EP_NUM++ ))
+        echo "${DATETIME} ${EP_NUM} ${URL} ${KEYWORD} ${SAVE_DIR_NUM} ${NUM_PREFIX} ${NUM_SUFFIX} ${SED_STR}" >> ${LIST_TEMP}
+    else
+        echo "${LAST_UPD} ${EP_NUM} ${URL} ${KEYWORD} ${SAVE_DIR_NUM} ${NUM_PREFIX} ${NUM_SUFFIX} ${SED_STR}" >> ${LIST_TEMP}
+    fi
     (( cnt++ ))
 done
 
