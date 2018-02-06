b565a9f4c025d655db18d1b2d561cd8d9e561380
swirhen <swirhen@gmail.com>
2018-02-06 15:17:35 +0900

    手慰み

---
+++ testutil.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# 各種疎通テストユーティリティ
+# 機能予定
+# ping
+# telnet
+# ntpdate
+# 各種多段sshから同様のテスト
+# ログのテール
+# ログのgrep
+# tmux-xpanesが入っていればマルチペイン化
+
+# yes/no
+yesno() {
+    echo -n" (Y/N)"
+    while read YESNO
+    do
+        case "${YESNO}" in
+            y | Y ) return y;;
+            n | N ) return n;;
+            * ) echo "prease imput Y or N(y or n)."
+            yesno
+        esac
+    done
+}
+
+echo "start,"
+yesno
+YNANSWER="$?"
+
+echo "YNANSWER: ${YNANSWER}"
