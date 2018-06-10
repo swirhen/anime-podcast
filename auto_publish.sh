8b24bd8b1027717aede1a4f4de7de1c8d771205b
swirhen <irankae1#>
2018-06-10 13:07:36 +0900

    debug

--- auto_publish.sh
+++ auto_publish.sh
@@ -193,7 +193,7 @@ do
       EPNUM_N=${EPNUM}
       if [ "${#EPNUM}" -gt 3 ]; then
         EPNUM=`echo "${title}" | sed "s/.*${NAME}.* \([0-9]\{2,3\}.5\) .*/\1/"`
-        expr ${EPNUM} + 1 > /dev/null 2>&1
+        expr "${EPNUM}" + 1 > /dev/null 2>&1
         if [ $? -ne 0 ]; then
           continue
         fi
