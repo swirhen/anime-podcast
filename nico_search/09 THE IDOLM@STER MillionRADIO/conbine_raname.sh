32262e4ea7340f704eca0c6a9884d4aeb7ae0cf3
swirhen <swirhen@gmail.com>
2018-07-06 17:09:23 +0900

    new: ビリビリ動画のアプリのダウンロードディレクトリから動画を生成する

---
+++ nico_search/09 THE IDOLM@STER MillionRADIO/conbine_raname.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+dir=$1
+
+# 与えられたディレクトリ以下のパートディレクトリの数を調べる
+dircnt=`ls ${dir} | wc -l`
+
+# パートディレクトリごとにループ
+for partdir in ${dir}/*
+do
+    # entry.jsonからtitleを取得
+    title=`cat ${dir}/${partdir}/entry.json | sed "s/.*title\":\"\(.*\)\",\"time.*/\1\n/"`
+    # entry.jsonからpartを取得
+    part=`cat ${dir}/${partdir}/entry.json | sed "s/.*part\":\"\(.*\)\",\"vid.*/\1\n/"`
+
+    # blv保存ディレクトリの下のblvファイル数を調べる
+    filecnt=`ls ${dir}/${partdir}/*/*.blv | wc -l`
+
+    # ファイル名=title
+    filename="${title}"
+    # 複数パートある場合はファイル名にpartを付加
+    if [ ${dircnt} -ne 1 ]; then
+        filename+=" ${part}"
+    fi
+
+    if [ ${filecnt} -eq 1 ]; then
+        # blvが1個だけの場合はリネーム
+        echo "mv ${dir}/${partdir}/*/0.blv \"${filename}\".mp4"
+        # mv ${dir}/${partdir}/*/0.blv "${filename}".mp4
+    else
+        # blvが複数ある場合はファイル名を連結
+        files=""
+        for file in ${dir}/${partdir}/*/*.blv
+        do
+            if [ "${files}" = "" ]; then
+                files="${file}"
+            else
+                files+="|${file}"
+            fi
+        done
+        # ffmpegでconcat
+        echo "/usr/bin/wine ffmpeg3.exe \"concat:${files}\ -c copy \"${filename}\".mp4"
+        # /usr/bin/wine ffmpeg3.exe "concat:${files}" -c copy "${filename}".mp4
+    fi
+done
