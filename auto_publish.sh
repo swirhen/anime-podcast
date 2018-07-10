2a325914a51d578a4bac6986e6d6c0fe300b75ce
swirhen <swirhen@gmail.com>
2018-07-10 17:43:42 +0900

    新番組変換システムの修正 デバッグ

--- auto_publish.sh
+++ auto_publish.sh
@@ -52,7 +52,7 @@ get_ja_title_list2() {
     echo "${TITLE_JA}"
 }
 
-end() {
+end_script() {
   mv ${LOG_FILE} ${SCRIPT_DIR}/logs/
   rm -f ${FLG_FILE}
   exit 0
@@ -350,7 +350,7 @@ download seeds:
     post_msg="swirhen.tv auto publish completed. (no new episode)"
     logging "${post_msg}"
     slack_post "${post_msg}"
-    end
+    end_script
   fi
 fi
 
@@ -438,4 +438,4 @@ slack_post "swirhen.tv auto publish completed."
 sleep 1
 slack_upload "${LOG_FILE}" "auto_publish_log_${DATETIME2}"
 
-end
+end_script
