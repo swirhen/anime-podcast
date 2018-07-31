40f8a0b4db5eb4343ffffa6cd92dc74c95f08109
swirhen <swirhen@gmail.com>
2018-07-31 12:31:46 +0900

    bilibili結合しぇる ファイル名対応リストからファイル名を変更する機能の実装

--- nico_search/bilibili_rename.sh
+++ nico_search/bilibili_rename.sh
@@ -1,6 +1,25 @@
 #!/usr/bin/env bash
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)"
+RENAME_LIST_FILE=${SCRIPT_DIR}/bilibili_rename_word.txt
 dir=$1
+
+KEYWORDS=()
+NUM_PREFIXS=()
+NUM_SUFFIXS=()
+PADDING_DIGITS=()
+PROGRAM_NAMES=()
+FILENAME_LAYOUTS=()
+FOOTER_KEYWORDS=()
+while read KEYWORD NUM_PREFIX NUM_SUFFIX PADDING_DIGIT PROGRAM_NAME FILENAME_LAYOUT FOOTER_KEYWORD
+do
+    KEYWORDS+=( "${KEYWORD}" )
+    NUM_PREFIXS+=( "${NUM_PREFIX}" )
+    NUM_SUFFIXS+=( "${NUM_SUFFIX}" )
+    PADDING_DIGITS+=( ${PADDING_DIGIT} )
+    PROGRAM_NAMES+=( "${PROGRAM_NAME//_/ }" )
+    FILENAME_LAYOUTS+=( "${FILENAME_LAYOUT//%/ }" )
+    FOOTER_KEYWORDS+=( "${FOOTER_KEYWORD}" )
+done < ${RENAME_LIST_FILE}
 
 # 与えられたディレクトリ以下のパートディレクトリの数を調べる
 dircnt=`ls "${dir}" | wc -l`
@@ -21,6 +40,36 @@ do
     # 複数パートある場合はファイル名にpartを付加
     if [ ${dircnt} -ne 1 ]; then
         filename+=" ${part}"
+    fi
+
+    cnt=0
+    hit_flg=0
+    while :
+    do
+        if [[ ${filename} =~ ${KEYWORDS[${cnt}]} ]]; then
+            NUM_PREFIX="${NUM_PREFIXS[${cnt}]}"
+            NUM_SUFFIX="${NUM_SUFFIXS[${cnt}]}"
+            PADDING_DIGIT="${PADDING_DIGITS[${cnt}]}"
+            PROGRAM_NAME="${PROGRAM_NAMES[${cnt}]}"
+            FILENAME_LAYOUT="${FILENAME_LAYOUTS[${cnt}]}"
+            FOOTER_KEYWORD="${FOOTER_KEYWORDS[${cnt}]}"
+            hit_flg=1
+            break
+        fi
+        (( cnt++ ))
+    done
+    if [ ${hit_flg} -eq 1 ]; then
+        NUM=`echo ${filename} | sed "s/.*${NUM_PREFIX}\([0-9]*\)${NUM_SUFFIX}.*/\1/"`
+        NUM=`printf %0${PADDING_DIGIT}d ${NUM}`
+        FILENAME_LAYOUT=`echo "${FILENAME_LAYOUT}" | sed "s/##PROGRAM_NAME##/${PROGRAM_NAME}/"`
+        FILENAME_LAYOUT=`echo "${FILENAME_LAYOUT}" | sed "s/##NUM_PREFIX##/${NUM_PREFIXS}/"`
+        FILENAME_LAYOUT=`echo "${FILENAME_LAYOUT}" | sed "s/##NUM_SUFFIX##/${NUM_SUFFIX}/"`
+        FILENAME_LAYOUT=`echo "${FILENAME_LAYOUT}" | sed "s/##NUM##/${NUM}/"`
+        if [ "${FOOTER_KEYWORD}" != "" ]; then
+            FOOTER=`echo ${filename} | sed "s/.*${FOOTER_KEYWORD}\(.*\)/\1/"`
+            FILENAME_LAYOUT+="${FOOTER}"
+        fi
+        filename="${FILENAME_LAYOUT}"
     fi
 
     if [ ${filecnt} -eq 1 ]; then
