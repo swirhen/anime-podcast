c3d14f6e10e1d0b040cb1193f4d93ce4bc0a9064
swirhen <swirhen@gmail.com>
2018-08-08 10:49:59 +0900

    debagu

--- nico_search/bilibili_rename.sh
+++ nico_search/bilibili_rename.sh
@@ -73,7 +73,7 @@ do
         FILENAME_LAYOUT=`echo "${FILENAME_LAYOUT}" | sed "s/##NUM_SUFFIX##/${NUM_SUFFIX}/"`
         FILENAME_LAYOUT=`echo "${FILENAME_LAYOUT}" | sed "s/##NUM##/${NUM}/"`
         if [ "${FOOTER_KEYWORD}" != "" ]; then
-            FOOTER=`echo ${filename} | sed "s/.*${FOOTER_KEYWORD}\(.*\)/\1/"`
+#            FOOTER=`echo ${filename} | sed "s/.*${FOOTER_KEYWORD}\(.*\)/\1/"`
             FILENAME_LAYOUT+=" ${FOOTER}"
         fi
         if [ "${DELETE_KEYWORD}" != "" ]; then
