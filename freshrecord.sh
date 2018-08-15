ecf5b375be4442a8e7f0f0b9f797e0821e3753ce
swirhen <swirhen@gmail.com>
2018-08-15 15:47:50 +0900

    Abema Fresh! 予約録画用スクリプト アーカイブ取得モード でばぐ

--- freshrecord.sh
+++ freshrecord.sh
@@ -87,10 +87,9 @@ done
 # ts を含む行が現れたら、ffmpegで録画開始
 filename="[FRESH LIVE] ${program_name} (${DATE}).mp4"
 if [ "${archive_flg}" = "1" ]; then
-    filename="[FRESH LIVE] ${program_name}.mp4"
+    filename="[FRESH LIVE] ${program_name//\//_}.mp4"
 fi
-/usr/bin/wine ffmpeg3.exe -i "${streamuri}" -c copy "${filename}"
-mv "${filename}" "${SAVE_DIR}"/
+/usr/bin/wine ffmpeg3.exe -i "${streamuri}" -c copy "${SAVE_DIR}"/"${filename}"
 
 # rssフィード生成シェル
 /data/share/movie/sh/mmmpc.sh agqr "超！A&G(+α)"
