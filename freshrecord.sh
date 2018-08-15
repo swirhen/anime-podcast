924cd7cc393c713be944d6af199e5ace83e6e94a
swirhen <swirhen@gmail.com>
2018-08-15 15:39:56 +0900

    Abema Fresh! 予約録画用スクリプト アーカイブ取得モード実装

--- freshrecord.sh
+++ freshrecord.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Abema Fresh! 予約録画用スクリプト
 # require: curl,ffmpeg
-# usage: freshrecord [番組名] [チャンネル名(URL)] [会員配信除外フラグ]
+# usage: freshrecord [番組名] [チャンネル名(URL)] [会員配信除外フラグ] [アーカイブフラグ]
 # しくみ:
 # https://freshlive.tv/[チャンネル名]/programs/upcoming をクロール
 # [番組名]の直近のもののIDを取得
@@ -14,6 +14,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)"
 name=$1
 channel=$2
 memberonly_ignore_flg=$3
+archive_flg=$4
 PYTHON_PATH="python3"
 DATETIME=`date +"%Y%m%d_%H%M"`
 DATETIME2=`date "+%Y%m%d%H%M%S"`
@@ -37,6 +38,10 @@ ${PYTHON_PATH} /home/swirhen/sh/slackbot/swirhentv/post.py "bot-open" "【FRESH 
 
 # https://freshlive.tv/[チャンネル名]/programs/upcoming をクロール
 # [番組名]の直近のもののIDを取得
+CRAWL_URI_SUFFIX="upcoming"
+if [ "${archive_flg}" = "1" ]; then
+    CRAWL_URI_SUFFIX="archive"
+fi
 logging "# 番組名:${name} (チャンネル: ${channel}) の放送URL取得開始"
 streaminfo=`curl https://freshlive.tv/${channel}/programs/upcoming | sed "s#<a href#\n<a href#g" | sed "s#</a>#</a>\n#g" | grep "^<a href=\"/${channel}/[0-9]" | grep title | grep -v "${ignoreword}" | sed "s#<a href=\"/${channel}/\([^\"]*\)\".*title=\"\([^\"]*\)\".*#\1|\2#" | grep "${name}" | head -1`
 
@@ -53,8 +58,12 @@ fi
 # https://movie.freshlive.tv/manifest/[ID]/live.m3u8 を取得
 # 最後の行(最高画質のもの)のURLを取得 (/playlist/DDDDDD.m3u8)
 # live.m3u8が有効になるのは 20分前なので、1分前に開始すること
+STREAM_URI_SUFFIX="live"
+if [ "${archive_flg}" = "1" ]; then
+    STREAM_URI_SUFFIX="archive"
+fi
 streamuri="https://movie.freshlive.tv"
-streamuri+=`curl "https://movie.freshlive.tv/manifest/${program_id}/live.m3u8" | tail -1`
+streamuri+=`curl "https://movie.freshlive.tv/manifest/${program_id}/${STREAM_URI_SUFFIX}.m3u8" | tail -1`
 
 logging "放送URL取得: ${streamuri}"
 
@@ -77,6 +86,9 @@ done
 
 # ts を含む行が現れたら、ffmpegで録画開始
 filename="[FRESH LIVE] ${program_name} (${DATE}).mp4"
+if [ "${archive_flg}" = "1" ]; then
+    filename="[FRESH LIVE] ${program_name}.mp4"
+fi
 /usr/bin/wine ffmpeg3.exe -i "${streamuri}" -c copy "${SAVE_DIR}"/"${filename}"
 
 # rssフィード生成シェル
