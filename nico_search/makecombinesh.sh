043ef82606cc65e6fde649dce0651e48b620cbb7
swirhen <swirhen@gmail.com>
2019-02-09 09:09:44 +0900

    debug

--- nico_search/makecombinesh.sh
+++ nico_search/makecombinesh.sh
@@ -5,4 +5,5 @@ for d in temp/${1}/*
 do
 /data/share/temp/voiceactor_nico_ch/bilibili_rename.sh "${d}" >> combine.sh
 done
+echo "/data/share/movie/sh/nico_search/nicorelease.sh" >> combine.sh
 cat combine.sh
