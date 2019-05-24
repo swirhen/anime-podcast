8b8f1537024d91a11b31a1b277f5bdc55765406f
swirhen <swirhen@gmail.com>
2019-05-24 11:13:48 +0900

    debug

--- backup.sh
+++ backup.sh
@@ -1,8 +1,8 @@
 # /bin/sh
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)"
 LOGFILE="${SCRIPT_DIR}/logs/daily_backup.log"
-echo "# DAILY BACKUP START: `date`" >> "${LOGFILE}"
-rsync -auv --delete /data8/temp8/ /data7/backup/temp8/ > "${LOGFILE}"
+echo "# DAILY BACKUP START: `date`" > "${LOGFILE}"
+rsync -auv --delete /data8/temp8/ /data7/backup/temp8/ >> "${LOGFILE}"
 rsync -auv --delete --exclude '.DAV' /data/share/book/ /data7/backup/book/ >> "${LOGFILE}"
 rsync -auv --delete --exclude '.DAV' --exclude='v20*' /data/share/temp/ /data7/backup/temp/ >> "${LOGFILE}"
 rsync -auv --delete --exclude '.DAV' /data8/movie8/ /data3/backup/ >> "${LOGFILE}"
--- nico_search/nicorelease.sh
+++ nico_search/nicorelease.sh
@@ -5,6 +5,7 @@ ln -s /data8/movie8/nico_search/0[^1389]*/*.mp4 /data/share/movie/98\ PSP用/nic
 ln -s /data8/movie8/nico_search/[1-8][0-9]*/*.mp4 /data/share/movie/98\ PSP用/nico/.
 ln -s /data8/movie8/nico_search/99*/*.mp4 /data/share/movie/98\ PSP用/nico/.
 ln -s /data8/movie8/nico_search/{01,03,08,09,98}*/*.mp4 /data/share/movie/98\ PSP用/nico_imas/.
+ln -s /data8/movie8/nico_search/20*/mp4/*.mp4 /data/share/movie/98\ PSP用/nico_imas/.
 /data/share/movie/sh/mmmpc.sh nico "ニコニコチャンネルの声優さん動画"
 /data/share/movie/sh/mmmpc.sh nico_imas "ニコニコチャンネルの声優さん動画(アイマス公式系)"
 find /data/share/movie/98\ PSP用/agqr/ -type l -exec rm {} \;
