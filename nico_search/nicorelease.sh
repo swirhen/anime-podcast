33c6ccc67e9ed9a78baf25802305f7037c84fd7e
swirhen <swirhen@gmail.com>
2019-06-21 15:53:12 +0900

    feed title no henkou

--- nico_search/nicorelease.sh
+++ nico_search/nicorelease.sh
@@ -6,8 +6,8 @@ ln -s /data8/movie8/nico_search/[1-8][0-9]*/*.mp4 /data/share/movie/98\ PSP用/n
 ln -s /data8/movie8/nico_search/99*/*.mp4 /data/share/movie/98\ PSP用/nico/.
 ln -s /data8/movie8/nico_search/{01,03,08,09,98}*/*.mp4 /data/share/movie/98\ PSP用/nico_imas/.
 ln -s /data8/movie8/nico_search/20*/mp4/*.mp4 /data/share/movie/98\ PSP用/nico_imas/.
-/data/share/movie/sh/mmmpc.sh nico "ニコニコチャンネルの声優さん動画"
-/data/share/movie/sh/mmmpc.sh nico_imas "ニコニコチャンネルの声優さん動画(アイマス公式系)"
+/data/share/movie/sh/mmmpc.sh nico "声優バラエティ動画"
+/data/share/movie/sh/mmmpc.sh nico_imas "声優バラエティ動画(アイマス公式系)"
 find /data/share/movie/98\ PSP用/agqr/ -type l -exec rm {} \;
 ln -s /data8/movie8/nico_search/(0[^1389]|[1-9][0-9])*/*.m4a /data/share/movie/98\ PSP用/agqr/
 /data/share/movie/sh/agqrrelease.sh
