8786beea8818a727bee8c3e231f2ebe16ef08153
swirhen <swirhen@gmail.com>
2019-10-07 17:54:37 +0900

    debug

--- auto_publish.sh
+++ auto_publish.sh
@@ -282,7 +282,15 @@ do
 #        link=`echo "cat /rss/channel/item[${cnt2}]" | xmllint --shell "${RSS_XML}" | grep link | sed "s#<link>\(.*\)</link>#\1#" | sed "s/^      //" | sed "s/amp;//"`
 #        TITLE_EN=`echo ${title} | sed "s/\[Leopard-Raws\]\ \(.*\)\ -\ 01\ RAW.*/\1/"`
 #        DL_HASH=`echo ${link} | sed "s/.*hash=\(.*\)/\1/"`
+        logging "title: ${title}"
+        if [ `echo "${title}" | grep "^w\ "` != "" ]; then
+            logging "# なんかバグ : title = ${title}"
+            title2=`echo ${title} | sed "s/^w\ \(.*\)/\1/"`
+            title="${title2}"
+        fi
+
         TITLE_EN=`echo ${title} | sed "s/\[.*\]\ \(.*\)\ -\ 01\ .*/\1/"`
+        logging "TITLE_EN: ${TITLE_EN}"
         if [[ "${title}" == "${TITLE_EN}" ]]; then
             logging "# なんかバグ : TITLE_EN = ${TITLE_EN}"
             continue
