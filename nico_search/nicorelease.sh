b769d04b4ec07bfb988746d1f08b7cd3cd7d2f17
swirhen <swirhen@gmail.com>
2020-01-09 05:18:36 +0900

    nl

--- nico_search/nicorelease.sh
+++ nico_search/nicorelease.sh
@@ -2,7 +2,7 @@
 rm -f /data/share/movie/98\ PSP用/nico/*.mp4
 rm -f /data/share/movie/98\ PSP用/nico_imas/*.mp4
 ln -s /data8/movie8/nico_search/0[^1389]*/*.mp4 /data/share/movie/98\ PSP用/nico/.
-ln -s /data8/movie8/nico_search/[1-8][0-9]*/*.mp4 /data/share/movie/98\ PSP用/nico/.
+ln -s /data8/movie8/nico_search/{21,22}*/*.mp4 /data/share/movie/98\ PSP用/nico/.
 ln -s /data8/movie8/nico_search/99*/*.mp4 /data/share/movie/98\ PSP用/nico/.
 ln -s /data8/movie8/nico_search/{01,03,08,09,98}*/*.mp4 /data/share/movie/98\ PSP用/nico_imas/.
 ln -s /data8/movie8/nico_search/20*/mp4/*.mp4 /data/share/movie/98\ PSP用/nico_imas/.
@@ -10,4 +10,5 @@ ln -s /data8/movie8/nico_search/20*/mp4/*.mp4 /data/share/movie/98\ PSP用/nico_
 /data/share/movie/sh/mmmpc.sh nico_imas "声優バラエティ動画(アイマス公式系)"
 find /data/share/movie/98\ PSP用/agqr/ -type l -exec rm {} \;
 ln -s /data8/movie8/nico_search/(0[^1389]|[1-9][0-9])*/*.m4a /data/share/movie/98\ PSP用/agqr/
+ln -s /data8/movie8/nico_search/20*/*.mp4 /data/share/movie/98\ PSP用/agqr/
 /data/share/movie/sh/agqrrelease.sh
