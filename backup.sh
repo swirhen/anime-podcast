83488bf26b239e65e16d8361179a634aadc5209a
swirhen <swirhen@gmail.com>
2020-04-12 10:27:29 +0900

    seiri

--- backup.sh
+++ backup.sh
@@ -2,8 +2,10 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)"
 LOGFILE="${SCRIPT_DIR}/logs/daily_backup.log"
 echo "# DAILY BACKUP START: `date`" > "${LOGFILE}"
+# temp8(アイマス系音楽&動画)
 rsync -auv --delete /data8/temp8/ /data7/backup/temp8/ >> "${LOGFILE}"
+# book(本) 
 rsync -auv --delete --exclude '.DAV' /data/share/book/ /data7/backup/book/ >> "${LOGFILE}"
-rsync -auv --delete --exclude '.DAV' --exclude='v20*' /data/share/temp/ /data7/backup/temp/ >> "${LOGFILE}"
-rsync -auv --delete --exclude '.DAV' /data8/movie8/ /data3/backup/ >> "${LOGFILE}"
+# nico_search(ニコニコ保存系)
+rsync -auv --delete --exclude '.DAV' /data8/movie8/nico_search/ /data3/backup/nico_search/ >> "${LOGFILE}"
 echo "# DAILY BACKUP END: `date`" >> "${LOGFILE}"
--- backup2.sh
+++
@@ -1,6 +0,0 @@
-# /bin/sh
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)"
-LOGFILE="${SCRIPT_DIR}/logs/data_backup.log"
-echo "# DAILY BACKUP2 START: `date`" > "${LOGFILE}"
-rsync -auv --delete --exclude 'lost+found' /data/ /data9/ >> "${LOGFILE}"
-echo "# DAILY BACKUP2 END: `date`" >> "${LOGFILE}"
