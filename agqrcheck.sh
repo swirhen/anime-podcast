161d4998e22da465d3cfc4302c471cd422c34be3
swirhen <swirhen@gmail.com>
2020-11-04 09:28:24 +0900

    debug

--- agqrcheck.sh
+++ agqrcheck.sh
@@ -2,26 +2,44 @@
 # 超A&G予約録画用・ストリーム有効性チェック
 # require: rtmpdump
 # ストリームURLの有効性をチェックする
-# aandg ファイルに書かれた文字列でチェックし、ダメならaandg2 ファイルのものをテスト
-# aandg2 ファイルで有効であれば、文字列入れ替え
-# 両方無効の場合はアラート
+# FILE [aandg2] でrtmp取得をチェック
+# FILE [aandg] でHLS取得をチェック
+# 無効の場合はアラート
+# 引数が指定された場合は定時報告する
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)"
 PYTHON_PATH="python3"
-#FILE=${SCRIPT_DIR}/aandg
+FILE_M3U8=${SCRIPT_DIR}/aandg
 FILE=${SCRIPT_DIR}/aandg2
 REC_FILE=${SCRIPT_DIR}/agqr_rec.mp4
 
-#num=`expr $RANDOM % 2 + 1`
-num=1
+okflg1=0
+okflg2=0
+
 PP=`cat ${FILE}`
-/usr/bin/rtmpdump --rtmp "rtmpe://fms${num}.uniqueradio.jp/" --playpath "${PP}" --app "?rtmp://fms-base2.mitene.ad.jp/agqr/" --live -o "${REC_FILE}" --stop 1
-#/usr/bin/wine ffmpeg3.exe -i "${PP}" -c copy -t 10 "${REC_FILE}"
+/usr/bin/rtmpdump --rtmp "rtmpe://fms1.uniqueradio.jp/" --playpath "${PP}" --app "?rtmp://fms-base2.mitene.ad.jp/agqr/" --live -o "${REC_FILE}" --stop 1
 
 if [ -s ${REC_FILE} ]; then
     rm -f ${REC_FILE}
-    exit 0
+    okflg1=1
 else
     rm -f ${REC_FILE}
-    /home/swirhen/tiasock/tiasock_common.sh "#anigera@w" "【超A&G チェック】録画にに失敗しました: ${PP}"
-    ${PYTHON_PATH} /home/swirhen/sh/slackbot/swirhentv/post.py "bot-open" "【超A&G チェック】録画に失敗しました: ${PP}"
+    /home/swirhen/tiasock/tiasock_common.sh "#anigera@w" "【超A&G チェック】RTMPでの録画にに失敗しました: ${PP}"
+    ${PYTHON_PATH} /home/swirhen/sh/slackbot/swirhentv/post.py "bot-open" "【超A&G チェック】RTMPでの録画に失敗しました: ${PP}"
+fi
+
+PLIST=`cat ${FILE_M3U8}`
+/usr/bin/wine ffmpeg3.exe -i "${PLIST}" -c copy -t 10 "${REC_FILE}"
+
+if [ -s ${REC_FILE} ]; then
+    rm -f ${REC_FILE}
+    okflg2=1
+else
+    rm -f ${REC_FILE}
+    /home/swirhen/tiasock/tiasock_common.sh "#anigera@w" "【超A&G チェック】HLSでの録画にに失敗しました: ${PLIST}"
+    ${PYTHON_PATH} /home/swirhen/sh/slackbot/swirhentv/post.py "bot-open" "【超A&G チェック】HLSでの録画に失敗しました: ${PLIST}"
+fi
+
+if [ "$1" != "" -a ${okflg1} -eq 1 -a ${okfkg2} -eq 1 ]; then
+    /home/swirhen/tiasock/tiasock_common.sh "#anigera@w" "【超A&G チェック 定時報告】RTMP / HLS 録画URLはともに有効です ${PP} / ${PLIST}"
+    ${PYTHON_PATH} /home/swirhen/sh/slackbot/swirhentv/post.py "bot-open" "【超A&G チェック 定時報告】RTMP / HLS 録画URLはともに有効です ${PP} / ${PLIST}"
 fi
