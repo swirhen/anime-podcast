3ed7d5ec2b8dfa878a00cfdcfcc06e02d51e7e3b
swirhen <swirhen@gmail.com>
2020-11-05 11:16:06 +0900

    debug

--- nico_search/makecombinesh.sh
+++ nico_search/makecombinesh.sh
@@ -9,7 +9,7 @@ for d in temp/${1}/*
 do
 /data/share/temp/voiceactor_nico_ch/bilibili_rename.sh "${d}" >> combine.sh
 done
-relfilg=`grep '*/"' combine.sh`
+relfilg=`grep "\*/$" combine.sh`
 if [ "${relfilg}" != "" ]; then
     echo "/data/share/movie/sh/nico_search/nicorelease.sh" >> combine.sh
 fi
