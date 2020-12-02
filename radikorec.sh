f9aac3c1f193d481e26eb683966e5b0fa59dbf53
swirhen <irankae1#>
2020-12-03 07:03:10 +0900

    debug

--- radikorec.sh
+++ radikorec.sh
@@ -1,144 +1,19 @@
 #!/bin/bash
-# Original1: https://gist.github.com/875864 saiten / rec_radiko.sh
-# Original2: http://backslash.ddo.jp/wordpress/archives/1020 http://backslash.ddo.jp/tools/rec_radiko2.txt 
+# Install: ffmpeg
 
-# Install: wget swftools rtmpdump ffmpeg http://d.hatena.ne.jp/zariganitosh/20130120/radiko_recoding_again
-
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)"
 PATH=$PATH:/usr/local/bin
 PYTHON_PATH="python3"
-VERSION=4.0.0
+TMP_PATH="/data/tmp"
 
 # 使い方
 show_usage() {
-  echo "Usage: $COMMAND [-a] [-o bangumi_mei] [-t recording_seconds] station_ID"
+  echo "Usage: $COMMAND [-a] [-o bangumi_mei] [-t recording_seconds] station_ID offset"
   echo '       -a  Output area info(ex. 'JP13,東京都,tokyo Japan'). No recording.'
-  echo '       -o  Default output_name = /data/tmp/[${station_name}]_`date +%Y%m%d-%H%M`.flv'
-  echo '             "hogehoge" = /data/tmp/[JOQR]hogehoge_20130123-1700.flv'
+  echo '       -o  Default output_name = /data/tmp/[${station_name}]_`date +%Y%m%d-%H%M`.m4a'
+  echo '             "hogehoge" = /data/tmp/[JOQR]hogehoge_20130123-1700.m4a'
   echo '       -t  Default recording_seconds = 30'
   echo '           60 = 1 minute, 3600 = 1 hour, 0 = go on recording until stopped(control-C)'
-}
-
-# 認証
-radiko_authorize() {
-  echo "==== authorize ===="
-  #
-  # get player
-  #
-  if [ ! -f $playerfile ]; then
-    echo $playerfile downloading...
-    /usr/bin/wget -q -O $playerfile $playerurl
-
-    if [ $? -ne 0 ]; then
-      echo "failed get player"
-      exit 1
-    fi
-  fi
-
-  #
-  # get keydata (need swftool)
-  #
-  if [ ! -f $keyfile ]; then
-    echo $keyfile extracting...
-    # swfextract -b 5 $playerfile -o $keyfile <---radiko仕様変更点
-    /usr/bin/swfextract -b 12 $playerfile -o $keyfile
-
-    if [ ! -f $keyfile ]; then
-      echo "failed get keydata"
-      exit 1
-    fi
-  fi
-
-  #
-  # access auth1_fms
-  #
-  wget -q \
-       --header="pragma: no-cache" \
-       --header="X-Radiko-App: pc_ts" \
-       --header="X-Radiko-App-Version: $VERSION" \
-       --header="X-Radiko-User: test-stream" \
-       --header="X-Radiko-Device: pc" \
-       --post-data='\r\n' \
-       --no-check-certificate \
-       --save-headers \
-       https://radiko.jp/v2/api/auth1_fms -O auth1_fms_$$
-
-  if [ $? -ne 0 ]; then
-    echo "failed auth1 process"
-    exit 1
-  fi
-
-  #
-  # get partial key
-  #
-  authtoken=`perl -ne 'print $1 if(/x-radiko-authtoken: ([\w-]+)/i)' auth1_fms_$$`
-  offset=`perl -ne 'print $1 if(/x-radiko-keyoffset: (\d+)/i)' auth1_fms_$$`
-  length=`perl -ne 'print $1 if(/x-radiko-keylength: (\d+)/i)' auth1_fms_$$`
-  partialkey=`dd if=$keyfile bs=1 skip=${offset} count=${length} 2> /dev/null | base64`
-
-  echo "authtoken: ${authtoken}\n offset: ${offset}\n length: ${length}\n partialkey: $partialkey"
-    
-  #
-  # access auth2_fms
-  #  
-  /usr/bin/wget -q \
-       --header="pragma: no-cache" \
-       --header="X-Radiko-App: pc_ts" \
-       --header="X-Radiko-App-Version: $VERSION" \
-       --header="X-Radiko-User: test-stream" \
-       --header="X-Radiko-Device: pc" \
-       --header="X-Radiko-Authtoken: ${authtoken}" \
-       --header="X-Radiko-Partialkey: ${partialkey}" \
-       --post-data='\r\n' \
-       --no-check-certificate \
-       https://radiko.jp/v2/api/auth2_fms -O auth2_fms_$$
-
-  if [ $? -ne 0 -o ! -f auth2_fms_$$ ]; then
-    echo "failed auth2 process"
-    exit 1
-  fi
-
-  echo "authentication success"
-
-  areaid=`/usr/bin/perl -ne 'print $1 if(/^([^,]+),/i)' auth2_fms_$$`
-  echo "areaid: $areaid"  
-}
-
-# 録音
-radiko_record() {
-  echo "==== recording ===="
-  #
-  # get stream-url
-  #
-  channel_xml=`/usr/bin/wget -q "http://radiko.jp/v2/station/stream/${channel}.xml" -O -`
-  stream_url=`echo $channel_xml | sed 's/^\(<?xml .*\)[Uu][Tt][Ff]8\(.* ?>\)/\1UTF-8\2/' | /usr/bin/xpath -e "//url/item[1]/text()" 2>/dev/null`
-  stream_url_parts=(`echo ${stream_url} | perl -pe 's!^(.*)://(.*?)/(.*)/(.*?)$/!$1://$2 $3 $4!'`)
-
-  #
-  # get authtoken
-  #
-  authtoken=`perl -ne 'print $1 if(/x-radiko-authtoken: ([\w-]+)/i)' auth1_fms_$$`
-
-  #
-  # rtmpdump
-  #
-  echo "save as '$output'"
-  echo "rtmpdump -r ${stream_url_parts[0]}
---app ${stream_url_parts[1]}
---playpath ${stream_url_parts[2]}
--W $playerurl
--C S: -C S: -C S: -C S:$authtoken
---live
---stop ${rectime:=30}
---flv ${output}"
- nohup rtmpdump -r ${stream_url_parts[0]} \
-           --app ${stream_url_parts[1]} \
-           --playpath ${stream_url_parts[2]} \
-           -W $playerurl \
-           -C S:"" -C S:"" -C S:"" -C S:$authtoken \
-           --live \
-           --stop "${rectime:=30}" \
-           --flv "${output}" &
-  pid=$!
 }
 
 # 引数解析
@@ -161,6 +36,10 @@ fi
 
 # オプション処理
 channel=$1
+offset=0
+if [ $2 != "" ]; then
+    offset=$2
+fi
 
 if [ "$OPTION_o" = "TRUE" ]; then
    pgmname="$VALUE_o"
@@ -170,54 +49,65 @@ if [ "$OPTION_t" = "TRUE" ]; then
   rectime=$VALUE_t
 fi
 
-cd /data/tmp ; wdir=`pwd`
-
 station_name=`curl -s "http://radiko.jp/v2/api/program/station/today?station_id=$channel" |/usr/bin/xpath -e "//station/name/text()" 2>/dev/null`
-output="${wdir}/${fname:=【${station_name}】${pgmname}_`date +%Y%m%d-%H%M`}.flv"
+# 保存ファイル名
+filename="${TMP_PATH}/【${station_name}】${pgmname}_`date +%Y%m%d-%H%M`}"
+# パスを除いたファイル名
+efilename="【${station_name}】${pgmname}_`date +%Y%m%d-%H%M`}"
 
-# playerurl=http://radiko.jp/player/swf/player_2.0.1.00.swf <---radiko仕様変更点
-#playerurl=http://radiko.jp/player/swf/player_$VERSION.swf
-playerurl=http://radiko.jp/apps/js/flash/myplayer-release.swf
-playerfile=./player.swf
-keyfile=./authkey.jpg
-
-if [ "$OPTION_a" = "TRUE" ]; then
-  radiko_authorize
-  if [ $? = 0 ]; then
-    cat auth2_fms_$$|grep -e '^\w\+'
-  fi
-else
 # つぶやく
 /home/swirhen/tiasock/tiasock_common.sh "#Twitter@t2" "【Radiko自動録音開始】${station_name} ${pgmname}"
-#/home/swirhen/tiasock/tiasock_common.sh "#Twitter@t2" "【Radiko自動録音開始】${fname}"
-${PYTHON_PATH} /home/swirhen/sh/slackbot/swirhentv/post.py "bot-open" "【Radiko自動録音開始】${fname}"
+${PYTHON_PATH} /home/swirhen/sh/slackbot/swirhentv/post.py "bot-open" "【Radiko自動録音開始】${efilename}"
 
-#until [ -f "${output}" ];
-#do
-#  radiko_authorize && radiko_record
-#done
+# プレイリストURL、トークン、エリア情報を取得
+PLINFO=( `${PYTHON_PATH} radikoauth.py ${channel}` )
+m3u8=${PLINFO[2]}
+token=${PLINFO[3]}
+area="${PLINFO[4]} ${PLINFO[5]}"
 
-radiko_authorize
-if [ $? = 0 ]; then
-  radiko_record
+if [ "$OPTION_a" = "TRUE" ]; then
+    echo "${area}"
+    exit 0
 fi
 
-sleep ${rectime}
-sleep 30
+# 録音時間に満たないファイルが生成されてしまった場合、続きから録音し直す(最終的に録音時間合計に達するまで続ける)
+rectime_rem=${rectime}
+file_num="01"
+until [ ${rectime_rem} -le 0 ]
+do
+    filename_rec="${filename}.${file_num}.m4a"
+    /usr/bin/wine ffmpeg3.exe -headers "X-Radiko-Authtoken:${token}" -i "${m3u8}" -c copy -t ${rectime_rem} "${filename_rec}"
+    mov_duration=`ffprobe -i "${filename_rec}" -select_streams v:0 -show_entries stream=duration 2>&1 | grep duration | sed s/duration=// | sed "s/\.[0-9]*$//g"`
+    if [ ${mov_duration} -ge ${rectime} ]; then
+        break
+    fi
+    rectime_rem=`expr ${rectime} - ${mov_duration}`
+    rectime=${rectime_rem}
+    file_num=$(( 10#${file_num} + 1 ))
+    file_num_zp="0${file_num}"
+    file_num="${file_num_zp: -2}"
+done
 
-kill ${pid}
+# 保存フォルダへ移動
+cd "${TMP_PATH}"
 
-/usr/bin/wine ffmpeg.exe -y -t ${rectime} -i "${output}" -acodec copy "/data/share/movie/98 PSP用/agqr/${fname}.m4a"
+# リスト作成
+rm -f "list_${efilename}"
+touch "list_${efilename}"
+for file in "${efilename}".*.m4a
+do
+    echo "file ${file}" >> "list_${efilename}"
+done
 
-rm -f "${output}"
+# 連結
+/usr/bin/wine ffmpeg3.exe -safe 0 -f concat -i "list_${efilename}" "/data/share/movie/98 PSP用/agqr/$efilename.m4a"
+
+# あとしまつ
+rm -f "list_${efilename}"
+rm -f "${efilename}".*.m4a
 
 # rssフィード生成シェル
 /data/share/movie/sh/mmmpc.sh agqr "超！A&G(+α)"
 # つぶやく
 /home/swirhen/tiasock/tiasock_common.sh "#Twitter@t2" "【Radiko自動録音終了】${station_name} ${pgmname}"
-#/home/swirhen/tiasock/tiasock_common.sh "#Twitter@t2" "【Radiko自動録音終了】${fname}"
-${PYTHON_PATH} /home/swirhen/sh/slackbot/swirhentv/post.py "bot-open" "【Radiko自動録音終了】${fname}"
-fi
-
-rm -f auth1_fms_$$
-rm -f auth2_fms_$$
+${PYTHON_PATH} /home/swirhen/sh/slackbot/swirhentv/post.py "bot-open" "【Radiko自動録音終了】${efilename}"
