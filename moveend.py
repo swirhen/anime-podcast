ff050ab17fbbe0040aa3fcd9d9432764275e0a7f
swirhen <irankae1#>
2021-04-16 18:31:59 +0900

    hogei

---
+++ moveend.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# 期またぎ移動用スクリプト
+import os,sys,re
+
+BASE_DIR = '/data/shar/movie'
+PSPMP4_98_DIR = BASE_DIR + '/98 PSP用'
+PSPMP4_MV_DIR = '/data2/movie2/pspmp4'
+ROOT_MV_DIR = '/data3/movie3'
+ROOT_MV_DIR_LINK = '/data/share/movie/0004 過去連載終了分'
+END_LIST_FILE = ''
+END_FILES = []
+YEAR = ''
+QUARTER = ''
+TARGET = ''
+PRG = ''
+CHECK = ''
+
+
+def askyear():
+    YEAR = input('Year? (YYYY)\n'
+                 'q: quit\n> ')
+    if re.match(r'^[1-2][0-9][0-9][0-9]$', YEAR):
+        return YEAR
+    elif YEAR == 'q' or YEAR == 'Q':
+        print('bye')
+        exit(0)
+    else:
+        print('Year is not valid.')
+        askyear()
+
+
+def askquarter():
+    QUARTER = input('Quarter? (1-4)\n'
+                    'q: quit\n> ')
+    if re.match(r'[1-4]$', QUARTER):
+        return QUARTER
+    elif QUARTER == 'q' or QUARTER == 'Q':
+        print('bye')
+        exit(0)
+    else:
+        print('Quarter is not valid.')
+        askquarter()
+
+
+def asktarget():
+    TARGET = input('Target?\n'
+                   '1: root(' + BASE_DIR + ')\n'
+                   '2: pspmp4(' + PSPMP4_98_DIR + ')\n'
+                   'q: quit\n> ')
+    if re.match(r'[1-2]$', TARGET):
+        return TARGET
+    elif TARGET == 'q' or TARGET == 'Q':
+        print('bye')
+        exit(0)
+    else:
+        print('Target is not valid.')
+        asktarget()
+
+
+def askprogress():
+    PROGRESS = input('Progress?\n'
+                     '1: move end program 2: remove symbolic link 3: check only\n'
+                     'q: quit\n> ')
+    if re.match(r'[1-3]$', PROGRESS):
+        return PROGRESS
+    elif PROGRESS == 'q' or PROGRESS == 'Q':
+        print('bye')
+        exit(0)
+    else:
+        print('Progress is not valid.')
+        askprogress()
+
+
+def askcheck():
+    CHECK = input('Check?\n'
+                  '0: off 1: on\n'
+                  'q: quit\n> ')
+    if re.match(r'[1-3]$', CHECK):
+        return CHECK
+    elif CHECK == 'q' or CHECK == 'Q':
+        print('bye')
+        exit(0)
+    else:
+        print('Check is not valid.')
+        askcheck()
+
+
+def askconfirm():
+    res = input('> ')
+    if res == 'y' or res == 'Y':
+        return 0
+    elif res == 'n' or res == 'N':
+        return 1
+    else:
+        print('y/nを入力してください。(EnterのみはNo)')
+        askconfirm()
+
+
+def waitenter():
+    input('(Enterで続行します)\n> ')
+
+args = sys.argv
+os.system('cls')
+
+if len(args) > 1:
+    YEAR = args[1]
+else:
+    YEAR = askyear()
+
+if len(args) > 2:
+    QUARTER = args[2]
+else:
+    QUARTER = askquarter()
+
+if len(args) > 3:
+    TARGET = args[3]
+else:
+    TARGET = asktarget()
+
+if len(args) > 4:
+    PROGRESS = args[4]
+else:
+    PROGRESS = askprogress()
+
+if len(args) > 5:
+    CHECK = args[5]
+else:
+    CHECK = askcheck()
+
+# askconfirm()
+# waitenter()
+
+print('YEAR: ' + YEAR + '\n'
+      'QUARTER: ' + QUARTER + '\n'
+      'TARGET: ' + TARGET + '\n'
+      'PROGRESS: ' + PROGRESS + '\n'
+      'CHECK: ' + CHECK + '\n')
