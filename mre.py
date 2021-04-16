ef2c34b4791c9b3f9f0dc09a986b9cb8c95f9fce
swirhen <irankae1#>
2021-04-16 14:43:19 +0900

    commit

--- mre.py
+++ mre.py
@@ -8,9 +8,12 @@ import os, glob, sys, pathlib,pprint,re
 
 current_dir = pathlib.Path(__file__).resolve().parent
 sys.path.append(str(current_dir) + '/python-lib/')
+LIST_FILE_PATH = str(current_dir) + '/checklist.txt'
 
-FILE_PATH = str(current_dir) + '/checklist.txt'
-
+SFX1 = '\ '
+SFX2 = '\ '
+print(SFX1)
+print(SFX2)
 args = sys.argv
 if len(args) > 1:
     SFX1 = args[1]
@@ -24,18 +27,44 @@ if len(args) > 1:
 
 # file open
 try:
-    f = open(FILE_PATH, 'r', encoding='utf-8')
+    listfile = open(LIST_FILE_PATH, 'r', encoding='utf-8')
 except Exception:
-    print("open error. not found file: ", str(FILE_PATH))
+    print("open error. not found file: ", str(LIST_FILE_PATH))
     sys.exit(1)
 
-for line in f:
-    if re.search('^Last\ Update', line):
+# make rename list
+renamelist = []
+for line in listfile:
+    if re.search('^Last Update', line):
         continue
-    line = re.sub(r'[^\ ]*\ .[^\ ]\ ', '', line)
-    # print(line)
+    line = re.sub(r'^[^ ]+ [^ ]+ ', '', line)
     line = line.strip().split("|")
-    nameE = line[0]
-    nameJ = line[1]
+    renamelist.append(line)
 
-    print(nameJ)
+pprint.pprint(renamelist)
+
+# make file list
+filelist = []
+filelist.extend(glob.glob("*.mp4") + glob.glob("*.mkv") + glob.glob("*.avi") + glob.glob("*.wmv"))
+
+# rename files
+for filename in filelist:
+    for name in renamelist:
+        nameE = name[0]
+        nameJ = name[1]
+        exp = r'.*(' + nameE + ').*' + SFX1 + '([1-9]{0,1}[0-9][0-9](.5)?)' + SFX2 + '.*\.(.*)'
+        name = re.sub(exp, r'\1', filename)
+        num = re.sub(exp, r'\2', filename)
+        ext = re.sub(exp, r'\4', filename)
+
+        if nameE == name:
+            if os.path.isfile(filename + '.aria2'):
+                print('#' + filename +' 成育中！')
+            else:
+                newname = nameJ + ' 第' + num + '話.' + ext
+                if filename != newname:
+                    print('# rename ' + filename + ' -> ' + newname)
+                    os.rename(filename, newname)
+                else:
+                    print('# 変更後のファイル名が同じ')
+            break
