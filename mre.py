505fbb3c7b3bb69959c78c23022ec71732493bd7
swirhen <irankae1#>
2021-04-16 10:43:04 +0900

    s

--- mre.py
+++ mre.py
@@ -6,11 +6,14 @@
 # 第1引数(省略可)はリネーム元ファイルの話数数字の前の文字を入力する。デフォルトは半角スペース。
 # 第2引数(省略可)はリネーム元ファイルの話数数字の後の文字を入力する。デフォルトは半角スペースもしくは、第1引数が指定されている場合は第1引数。
 
-import os,glob,sys,pathlib
+import os, glob, sys, pathlib
 
 current_dir = pathlib.Path(__file__).resolve().parent
 sys.path.append(str(current_dir) + '/../python-lib/')
 
+import pprint
+pprint.pprint(sys.path)
+
 import util
 
-print (file_open('checklist.txt'))
+print(file_open('checklist.txt'))
