dd2b6c5381869b3de373c08a5a732df669f33c9f
swirhen <irankae1#>
2021-04-16 11:41:27 +0900

    s

--- mre.py
+++ mre.py
@@ -1,19 +1,35 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # 動画リネーム用シェル
-# リネーム用リストにはリネーム元ファイルの検索文字と正式な作品名を
-# tabで繋げて記述すること
 # 第1引数(省略可)はリネーム元ファイルの話数数字の前の文字を入力する。デフォルトは半角スペース。
 # 第2引数(省略可)はリネーム元ファイルの話数数字の後の文字を入力する。デフォルトは半角スペースもしくは、第1引数が指定されている場合は第1引数。
 
-import os, glob, sys, pathlib
+import os, glob, sys, pathlib,pprint,re
 
 current_dir = pathlib.Path(__file__).resolve().parent
 sys.path.append(str(current_dir) + '/python-lib/')
 
-import pprint
-pprint.pprint(sys.path)
+FILE_PATH = str(current_dir) + '/checklist.txt'
 
-import util
+args = sys.argv
+# if len(args) > 1:
+#     print(args[0])
+#     print(args[1])
 
-print(util.file_open('checklist.txt'))
+# file open
+try:
+    f = open(FILE_PATH, 'r', encoding='utf-8')
+except Exception:
+    print("open error. not found file: ", str(FILE_PATH))
+    sys.exit(1)
+
+for line in f:
+    if re.search('^Last\ Update', line):
+        continue
+    line = re.sub(r'[^\ ]*\ .[^\ ]\ ', '', line)
+    # print(line)
+    line = line.strip().split("|")
+    nameE = line[0]
+    nameJ = line[1]
+
+    print(nameJ)
