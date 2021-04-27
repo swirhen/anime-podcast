cc5a53b37cb96614b5247754cb71973749142668
swirhen <swirhen@gmail.com>
2021-04-27 17:56:26 +0900

    debug

--- nico_search/nicorelease.sh
+++ nico_search/nicorelease.sh
@@ -3,7 +3,7 @@ rm -f /data/share/movie/98\ PSP用/nico/*.mp4
 rm -f /data/share/movie/98\ PSP用/nico_imas/*.mp4
 ln -s /data8/movie8/nico_search/0[^1389]*/*.mp4 /data/share/movie/98\ PSP用/nico/.
 ln -s /data8/movie8/nico_search/1[0-9]*/*.mp4 /data/share/movie/98\ PSP用/nico/.
-ln -s /data8/movie8/nico_search/{21,22}*/*.mp4 /data/share/movie/98\ PSP用/nico/.
+ln -s /data8/movie8/nico_search/2[1-3]*/*.mp4 /data/share/movie/98\ PSP用/nico/.
 ln -s /data8/movie8/nico_search/99*/*.mp4 /data/share/movie/98\ PSP用/nico/.
 ln -s /data8/movie8/nico_search/{01,03,08,09,98}*/*.mp4 /data/share/movie/98\ PSP用/nico_imas/.
 ln -s /data8/movie8/nico_search/20*/mp4/*.mp4 /data/share/movie/98\ PSP用/nico_imas/.
