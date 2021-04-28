35f7429b36d5f95163ed306c44b1ed109d39a9da
swirhen <irankae1#>
2021-04-28 10:48:04 +0900

    aria2c call test

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-val = input('nanika iretene: ')
-print(val + '!!!!!!!!')
+import pathlib
+import subprocess
+
+current_dir = pathlib.Path(__file__).resolve().parent
+SCRIPT_DIR = str(current_dir)
+DOWNLOAD_DIR = '/data/share/temp'
+
+subprocess.call(['aria2c', '--listen-port=38888', '--max-upload-limit=200K', '--seed-ratio=0.01', '--seed-time=1 ', DOWNLOAD_DIR + '/*.torrent'])
