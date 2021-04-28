1ad727ba1d1867ee593bd6ef426521ce1bc173df
swirhen <irankae1#>
2021-04-28 11:40:35 +0900

    aria2c call test

--- test.py
+++ test.py
@@ -17,7 +17,8 @@ DOWNLOAD_DIR = '/data/share/temp'
 TDATETIME = dt.now()
 DATETIME = TDATETIME.strftime('%Y/%m/%d-%H:%M:%S')
 
-seedlist = glob.glob(DOWNLOAD_DIR + '/*.torrent')
+os.chdir(DOWNLOAD_DIR)
+seedlist = glob.glob('*.torrent')
 if len(seedlist) == 0:
     print('seed file not found: ' + DOWNLOAD_DIR)
     exit(1)
@@ -27,7 +28,6 @@ post_msg='torrent douwnload: 以下のファイルの栽培を開始\n' + \
          '```' + '\n'.join(seedlist) + '```'
 swutil.slack_post('bot-sandbox', post_msg)
 
-os.chdir(DOWNLOAD_DIR)
 proc = subprocess.Popen('aria2c --listen-port=38888 --max-upload-limit=200K --seed-ratio=0.01 --seed-time=1 *.torrent', shell=True)
 
 time.sleep(10)
