a6710be343e3be0a1d37a6182b568feb19b8ce24
swirhen <irankae1#>
2021-04-28 10:51:33 +0900

    test

--- test.py
+++ test.py
@@ -7,4 +7,5 @@ current_dir = pathlib.Path(__file__).resolve().parent
 SCRIPT_DIR = str(current_dir)
 DOWNLOAD_DIR = '/data/share/temp'
 
-subprocess.call(['aria2c', '--listen-port=38888', '--max-upload-limit=200K', '--seed-ratio=0.01', '--seed-time=1 ', DOWNLOAD_DIR + '/*.torrent'])
+# subprocess.call(['aria2c', '--listen-port=38888', '--max-upload-limit=200K', '--seed-ratio=0.01', '--seed-time=1 ', DOWNLOAD_DIR + '/*.torrent'])
+subprocess.call('aria2c --listen-port=38888 --max-upload-limit=200K --seed-ratio=0.01 --seed-time=1 ' + DOWNLOAD_DIR + '/*.torrent', shell=True)
