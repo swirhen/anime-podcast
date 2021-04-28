7b8d2e86a000aa4fa42ec0f99e1dbf6233fa3bf1
swirhen <irankae1#>
2021-04-28 10:54:31 +0900

    aria2c call test

--- test.py
+++ test.py
@@ -8,4 +8,4 @@ SCRIPT_DIR = str(current_dir)
 DOWNLOAD_DIR = '/data/share/temp'
 
 # subprocess.call(['aria2c', '--listen-port=38888', '--max-upload-limit=200K', '--seed-ratio=0.01', '--seed-time=1 ', DOWNLOAD_DIR + '/*.torrent'])
-subprocess.call('aria2c --listen-port=38888 --max-upload-limit=200K --seed-ratio=0.01 --seed-time=1 ' + DOWNLOAD_DIR + '/*.torrent', shell=True)
+subprocess.Popen('aria2c --listen-port=38888 --max-upload-limit=200K --seed-ratio=0.01 --seed-time=1 ' + DOWNLOAD_DIR + '/*.torrent', shell=True)
