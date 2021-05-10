2ba246d7e575c426e1f7360d682bc288179f709e
swirhen <irankae1#>
2021-05-10 18:21:16 +0900

    debug

--- mmmpc.py
+++ mmmpc.py
@@ -17,7 +17,7 @@ TARGET_DIR_PARENT = str(current_dir) + '/../98 PSP用'
 if __name__ == '__main__':
     args = sys.argv
     if len(args) == 3:
-        feed_directory = TARGET_DIR_PARENT + args[1]
+        feed_directory = TARGET_DIR_PARENT + '/' + args[1]
         feed_title = args[2]
         if pathlib.Path(feed_directory).is_dir():
             swiutil.make_feed_manually(feed_directory, feed_title)
