949ea584234cf065f5834ad2b5362e53753e6ecf
swirhen <irankae1#>
2021-05-19 18:31:20 +0900

    debug

--- python-lib/slackbot_run.py
+++ python-lib/slackbot_run.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 import os
+import pathlib
 from slackbot.bot import Bot
+current_dir = pathlib.Path(__file__).resolve().parent
+SCRIPT_DIR = str(current_dir)
 
 
 def main():
     pid = os.getpid()
-    with open(os.getcwd() + '/slackbot.pid', 'w') as pidfile:
+    with open(f'{SCRIPT_DIR}/slackbot.pid', 'w') as pidfile:
         pidfile.write(str(pid))
     bot = Bot()
     bot.run()
