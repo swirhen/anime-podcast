3d7970d94ae4a957785e71df546571bcbe8443b3
swirhen <irankae1#>
2021-05-21 10:14:18 +0900

    discord bot hands on

---
+++ python-lib/discord_bot.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import pathlib
+import discord
+
+# 自分のBotのアクセストークンに置き換えてください
+current_dir = pathlib.Path(__file__).resolve().parent
+with open(f'{str(current_dir)}/discord_token') as tokenfile:
+    TOKEN = tokenfile.read().splitlines()
+
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
+
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
+
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
+
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
