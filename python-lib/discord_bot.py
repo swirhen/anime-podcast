1fc9357e38986ea1ca46478cf302b2431a724f13
swirhen <irankae1#>
2021-05-27 11:40:46 +0900

    debug

--- python-lib/discord_bot.py
+++ python-lib/discord_bot.py
@@ -4,8 +4,6 @@ import sys
 import os
 import pathlib
 import re
-from datetime import datetime as dt
-import datetime
 import discord
 import bot_util as bu
 import swirhentv_util as swiutil
@@ -37,6 +35,8 @@ async def on_ready():
 # メッセージ受信時に動作する処理
 @client.event
 async def on_message(message):
+    date_time = bu.get_now_datetime_str('YMDHMS')
+
     # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
         return
@@ -46,15 +46,13 @@ async def on_message(message):
 
     # 種情報
     elif re.search('^/seed.*', message.content):
-        tdatetime = dt.now()
-        date_time = tdatetime.strftime('%Y%m%d%H%M%S')
-        result_file_name = f'{SCRIPT_DIR}/seed_info_{date_time}.txt'
         past_days = 3
         if len(message.content.split()) > 1:
             past_days = int(message.content.split()[1])
         await message.channel.send(f'あつめた種の情報をおしらせするよ(さいきん {past_days} にちぶん)')
         result = bu.get_seed_directory(past_days)
         if len(result) > 2000:
+            result_file_name = f'{SCRIPT_DIR}/seed_info_{date_time}.txt'
             swiutil.writefile_new(result_file_name, result)
             await message.channel.send(file=discord.File(result_file_name))
             os.remove(result_file_name)
@@ -63,18 +61,12 @@ async def on_message(message):
 
     # 種リスト
     elif re.search('^/tl.*', message.content):
-        tdatetime = dt.now()
-        date_time = tdatetime.strftime('%Y%m%d%H%M%S')
-        result_file_name = f'{SCRIPT_DIR}/seed_list_{date_time}.txt'
-        target_category = ''
         arguments = message.content.split()
+        target_category = ''
         if len(arguments) > 1:
             target_category = arguments[1]
         else:
-            await message.channel.send('つかいかた(´・ω・`)\n'
-                                       'tl [たいしょうカテゴリ]\n'
-                                       'カテゴリ: doujin/manga/music/comic/live\n'
-                                       '(どうじん/えろまんが/おんがく/いっぱんまんが/らいぶ)')
+            await message.channel.send(bu.generate_message('usage_report_seed_list'))
             return
 
         seed_list = trsc.get_seed_list(target_category)
@@ -85,6 +77,7 @@ async def on_message(message):
             for seed in seed_list:
                 result += f'{seed[1]}\n'
             if len(result) > 2000:
+                result_file_name = f'{SCRIPT_DIR}/seed_list_{date_time}.txt'
                 swiutil.writefile_new(result_file_name, result)
                 await message.channel.send(file=discord.File(result_file_name))
                 os.remove(result_file_name)
@@ -95,9 +88,6 @@ async def on_message(message):
 
     # 種サーチ
     elif re.search('^/ts.*', message.content):
-        tdatetime = dt.now()
-        date_time = tdatetime.strftime('%Y%m%d%H%M%S')
-        result_file_name = f'{SCRIPT_DIR}/seed_search_{date_time}.txt'
         keyword = ''
         target_category = 'all'
         arguments = message.content.split()
@@ -106,9 +96,7 @@ async def on_message(message):
             if len(arguments) > 2:
                 target_category = arguments[2]
         else:
-            await message.channel.send('つかいかた(´・ω・`)\nts [けんさくキーワード] [たいしょうカテゴリ]\n'
-                                       'カテゴリ: doujin/manga/music/comic/live/all\n'
-                                       '(どうじん/えろまんが/おんがく/いっぱんまんが/らいぶ/ぜんぶ)')
+            await message.channel.send(bu.generate_message('usage_torrent_search'))
             return
 
         await message.channel.send('さがしてくるよ(｀・ω・´)\n'
@@ -120,6 +108,7 @@ async def on_message(message):
             if len(result) > 2000:
                 await message.channel.send('みつかったよ(｀・ω・´)')
                 result_mod = result.replace('みつかったよ(｀・ω・´)\n```', '').replace('```', '')
+                result_file_name = f'{SCRIPT_DIR}/seed_search_{date_time}.txt'
                 swiutil.writefile_new(result_file_name, result_mod)
                 await message.channel.send(file=discord.File(result_file_name))
                 os.remove(result_file_name)
@@ -128,9 +117,6 @@ async def on_message(message):
 
     # 種移動のみ
     elif re.search('^/tmv.*', message.content):
-        tdatetime = dt.now()
-        date_time = tdatetime.strftime('%Y%m%d%H%M%S')
-        result_file_name = f'{SCRIPT_DIR}/seed_move_{date_time}.txt'
         arguments = message.content.split()
         seed_dir = ''
         target_dir = ''
@@ -141,26 +127,12 @@ async def on_message(message):
             if len(arguments) > 3:
                 keyword = arguments[3]
         else:
-            await message.channel.send('つかいかた(´・ω・`)\n'
-                                       'tdl [たねのあるディレクトリ] [いどうさきのディレクトリ] [いどうするたねをしぼりこむキーワード]\n'
-                                       'いどうもとディレクトリ: ひづけ(YYYYMMDD) もしくは t(きょうのひづけ)\n'
-                                       'いどうさきディレクトリ:\n'
-                                       'd: どうじん c: みせいりほん m: えろまんが\n'
-                                       'cm: でれおんがく cl: でれらいぶ\n'
-                                       'mm: みりおんがく ml:みりらいぶ\n'
-                                       'sm:しゃにおんがく sl:しゃにらいぶ\n'
-                                       'hm:ほろおんがく hl:ほろらいぶ\n'
-                                       'もしくは ふるぱすもじれつ')
+            await message.channel.send(bu.generate_message('usage_torrent_move'))
+            return
 
         target_dir = bu.choose_target_dir(target_dir)
         if target_dir == '':
-            message.send('いどうさきディレクトリ:\n'
-                         'd: どうじん c: みせいりほん m: えろまんが\n'
-                         'cm: でれおんがく cl: でれらいぶ\n'
-                         'mm: みりおんがく ml:みりらいぶ\n'
-                         'sm:しゃにおんがく sl:しゃにらいぶ\n'
-                         'hm:ほろおんがく hl:ほろらいぶ\n'
-                         'もしくは ふるぱすもじれつ')
+            await message.channel.send(bu.generate_message('usage_torrent_move_directory_choice'))
             return
 
         result = bu.seed_move(seed_dir, target_dir, keyword)
@@ -173,6 +145,7 @@ async def on_message(message):
             await message.channel.send('いどうしたたね:')
             result = '\n'.join(seedlist)
             if len(result) > 2000:
+                result_file_name = f'{SCRIPT_DIR}/seed_move_{date_time}.txt'
                 swiutil.writefile_new(result_file_name, result)
                 await message.channel.send(file=discord.File(result_file_name))
                 os.remove(result_file_name)
@@ -181,9 +154,6 @@ async def on_message(message):
 
     # 種移動&栽培
     elif re.search('^/tdl.*', message.content):
-        tdatetime = dt.now()
-        date_time = tdatetime.strftime('%Y%m%d%H%M%S')
-        result_file_name = f'{SCRIPT_DIR}/seed_move_{date_time}.txt'
         arguments = message.content.split()
         seed_dir = ''
         target_dir = ''
@@ -194,26 +164,12 @@ async def on_message(message):
             if len(arguments) > 3:
                 keyword = arguments[3]
         else:
-            await message.channel.send('つかいかた(´・ω・`)\n'
-                                       'tdl [たねのあるディレクトリ] [いどうさきのディレクトリ] [いどうするたねをしぼりこむキーワード]\n'
-                                       'いどうもとディレクトリ: ひづけ(YYYYMMDD) もしくは t(きょうのひづけ)\n'
-                                       'いどうさきディレクトリ:\n'
-                                       'd: どうじん c: みせいりほん m: えろまんが\n'
-                                       'cm: でれおんがく cl: でれらいぶ\n'
-                                       'mm: みりおんがく ml:みりらいぶ\n'
-                                       'sm:しゃにおんがく sl:しゃにらいぶ\n'
-                                       'hm:ほろおんがく hl:ほろらいぶ\n'
-                                       'もしくは ふるぱすもじれつ')
+            await message.channel.send(bu.generate_message('usage_torrent_move'))
+            return
 
         target_dir = bu.choose_target_dir(target_dir)
         if target_dir == '':
-            message.send('いどうさきディレクトリ:\n'
-                         'd: どうじん c: みせいりほん m: えろまんが\n'
-                         'cm: でれおんがく cl: でれらいぶ\n'
-                         'mm: みりおんがく ml:みりらいぶ\n'
-                         'sm:しゃにおんがく sl:しゃにらいぶ\n'
-                         'hm:ほろおんがく hl:ほろらいぶ\n'
-                         'もしくは ふるぱすもじれつ')
+            await message.channel.send(bu.generate_message('usage_torrent_move_directory_choice'))
             return
 
         result = bu.seed_move(seed_dir, target_dir, keyword)
@@ -228,6 +184,7 @@ async def on_message(message):
             await message.channel.send('いどうしたたね:')
             result = '\n'.join(seedlist)
             if len(result) > 2000:
+                result_file_name = f'{SCRIPT_DIR}/seed_move_{date_time}.txt'
                 swiutil.writefile_new(result_file_name, result)
                 await message.channel.send(file=discord.File(result_file_name))
                 os.remove(result_file_name)
@@ -251,63 +208,16 @@ async def on_message(message):
             nick_flg = arguments[5]
             your_nick_ignore_flg = arguments[6]
         else:
-            await message.channel.send('つかいかた(´・ω・`)\n'
-                                       'tws [けんさくキーワード or twitterid] [チャンネル] [けんさくかいしにちじ] [けんさくしゅうりょうにちじ] [twitteridでけんさく] [じぶんのtwitteridをむしする]\n'
-                                       'チャンネル: y/s/k/e/f/c/m/h/ha\n'
-                                       '(ゆうめいじん/せいゆう/かくげーぜい/えし/おともだち/いちもん/いちざ/ほろ/ほろのえ)\n'
-                                       'けんさくかいしにちじ: YYYY-MM-DD HH:MM:SSけいしき\n'
-                                       'もしくは [なんふんまえ]m/[なんじかんまえ]h/[なんにちまえ]d\n'
-                                       'けんさくしゅうりょうにちじ: nowといれたら げんざいにちじ\n'
-                                       'twitteridでけんさく: 0: キーワードけんさく 1: twitteridでけんさく\n'
-                                       'じぶんのtwitteridをむしする: 0: むしする 1: むししない')
+            await message.channel.send(bu.generate_message('usage_twitter_search'))
             return
 
-        if channel == 'y':
-            channel = '#Twitter有名人@t'
-        elif channel == 's':
-            channel = '#twitter声優@t'
-        elif channel == 'k':
-            channel = '#twitter格ゲー@t'
-        elif channel == 'e':
-            channel = '#twitter絵描きさん@t'
-        elif channel == 'f':
-            channel = '#おともだちtwitter@t'
-        elif channel == 'c':
-            channel = '#シンデレラ一門@t'
-        elif channel == 'm':
-            channel = '#ミリオン一座@t'
-        elif channel == 'h':
-            channel = '#hololive@t'
-        elif channel == 'ha':
-            channel = '#holoart@t'
-        else:
-            message.send('チャンネル: y/s/k/e/f/c/m/h/ha\n'
-                         '(ゆうめいじん/せいゆう/かくげーぜい/えし/おともだち/いちもん/いちざ/ほろ/ほろのえ)\n')
-            return 1
-    
-        now_time = dt.now()
-        now_datetime = dt.now().strftime('%Y/%m/%d %H:%M:%S')
-        if since[-1] == 'm':
-            shift_minutes = int(since[:-1])
-            since = (now_time - datetime.timedelta(minutes=int(shift_minutes))).strftime('%Y/%m/%d %H:%M:%S')
-        elif since[-1] == 'h':
-            shift_hours = int(since[:-1])
-            since = (now_time - datetime.timedelta(hours=int(shift_hours))).strftime('%Y/%m/%d %H:%M:%S')
-        elif since[-1] == 'd':
-            shift_days = int(since[:-1])
-            since = (now_time - datetime.timedelta(days=int(shift_days))).strftime('%Y/%m/%d %H:%M:%S')
-    
-        if until[-1] == 'm':
-            shift_minutes = int(until[:-1])
-            until = (now_time - datetime.timedelta(minutes=int(shift_minutes))).strftime('%Y/%m/%d %H:%M:%S')
-        elif until[-1] == 'h':
-            shift_hours = int(until[:-1])
-            until = (now_time - datetime.timedelta(hours=int(shift_hours))).strftime('%Y/%m/%d %H:%M:%S')
-        elif until[-1] == 'd':
-            shift_days = int(until[:-1])
-            until = (now_time - datetime.timedelta(days=int(shift_days))).strftime('%Y/%m/%d %H:%M:%S')
-        elif until == 'now':
-            until = now_datetime
+        channel = bu.choose_channel(channel)
+        if channel == '':
+            await message.channel.send(bu.generate_message('usage_twitter_search_channel_choice'))
+            return
+
+        since = bu.get_now_datetime_str('YMD_HMS', since)
+        until = bu.get_now_datetime_str('YMD_HMS', until)
     
         k_n_str = 'キーワード'
         k_n_i_str = 'むしする'
@@ -332,8 +242,6 @@ async def on_message(message):
         if len(result) > 0:
             await message.channel.send('みつかったにぇ！(｀・ω・´)')
             if len(result) > 2000:
-                tdatetime = dt.now()
-                date_time = tdatetime.strftime('%Y%m%d%H%M%S')
                 result_file_name = f'{SCRIPT_DIR}/twitter_search_{date_time}.txt'
                 swiutil.writefile_new(result_file_name, result.replace('`',''))
                 await message.channel.send(file=discord.File(result_file_name))
