6eebc97960acb8e69895a6ddc429847569432af0
swirhen <irankae1#>
2021-05-27 16:44:21 +0900

    debug

--- python-lib/discord_bot_ztb.py
+++ python-lib/discord_bot_ztb.py
@@ -46,7 +46,7 @@ async def on_message(message):
 
 if __name__ == "__main__":
     pid = os.getpid()
-    with open(f'{SCRIPT_DIR}/discordbot.pid', 'w') as pidfile:
+    with open(f'{SCRIPT_DIR}/discordbot_ztb.pid', 'w') as pidfile:
         pidfile.write(str(pid))
     # Botの起動とDiscordサーバーへの接続
     client.run(TOKEN)
