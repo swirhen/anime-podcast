342897e6d55ebb686a2eb19343ecfae6f1d85397
swirhen <irankae1#>
2021-05-27 16:39:57 +0900

    zatsuba you bot kaihatu kaisi

---
+++ python-lib/discord_bot_ztb.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+# import sections
+import sys
+import os
+import pathlib
+import re
+import discord
+import bot_util as bu
+import swirhentv_util as swiutil
+sys.path.append('/home/swirhen/sh/checker/torrentsearch')
+import torrentsearch as trsc
+
+# argument section
+SHARE_TEMP_DIR = '/data/share/temp'
+SEED_DOWNLOAD_DIR = f'{SHARE_TEMP_DIR}/torrentsearch'
+SEED_BACKUP_DIR = f'{SHARE_TEMP_DIR}/torrentsearch/downloaded'
+DL_URL_LIST_FILE = f'/home/swirhen/sh/checker/torrentsearch/download_url.txt'
+current_dir = pathlib.Path(__file__).resolve().parent
+SCRIPT_DIR = str(current_dir)
+with open(f'{SCRIPT_DIR}/discord_token') as tokenfile:
+    TOKEN = tokenfile.read().splitlines()[0]
+
+
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
+
+
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
+
+
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+    date_time = bu.get_now_datetime_str('YMDHMS')
+
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
+
+if __name__ == "__main__":
+    pid = os.getpid()
+    with open(f'{SCRIPT_DIR}/discordbot.pid', 'w') as pidfile:
+        pidfile.write(str(pid))
+    # Botの起動とDiscordサーバーへの接続
+    client.run(TOKEN)
