463cf50889686b0e25a7bd703805bbf292b6e0a8
swirhen <irankae1#>
2021-05-28 15:12:27 +0900

    debug

--- python-lib/discord_bot.py
+++ python-lib/discord_bot.py
@@ -44,6 +44,9 @@ async def on_message(message):
     if message.content == '/neko':
         await message.channel.send('にゃーん')
 
+    elif message.content == '/kokodoko':
+        await message.channel.send(message.channel.name)
+
     # 種情報
     elif re.search('^/seed.*', message.content):
         past_days = 3
