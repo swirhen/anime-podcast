651189466cf84de660dab85d732275a66cbfe48d
swirhen <irankae1#>
2021-05-28 10:53:40 +0900

    debug

--- python-lib/jihou.py
+++ python-lib/jihou.py
@@ -12,30 +12,32 @@ NEYOU_FILE = f'{SCRIPT_DIR}/neyou.jpg'
 
 if __name__ == '__main__':
     args = sys.argv
+    here_txt = '@here '
     if len(args) > 1:
         debug = True
         debug_arg = int(args[1])
+        here_txt = ''
         debug_str = ' (debugちゅう)'
 
     hour = int(bu.get_now_datetime_str('H'))
     date = bu.get_now_datetime_str('YMD_A')
     if hour == 0 or debug_arg == 0:
         date = bu.get_now_datetime_str('YMD_A')
-        post_str = f'@here {date} になりました。 {str(hour)} 時ごろをお知らせします。'
+        post_str = f'{here_txt}{date} になりました。 {str(hour)} 時ごろをお知らせします。'
     if hour == 0 or debug_arg == 0:
         date = bu.get_now_datetime_str('YMD_A')
-        post_str = f'@here {date} になりました。 {str(hour)} 時ごろをお知らせします。'
+        post_str = f'{here_txt}{date} になりました。 {str(hour)} 時ごろをお知らせします。'
     elif hour == 12 or debug_arg == 12:
-        post_str = f'@here {hour} 時ごろをお知らせします。おひるです。'
+        post_str = f'{here_txt}{hour} 時ごろをお知らせします。おひるです。'
     elif hour == 15 or debug_arg == 15:
-        post_str = f'@here {hour} 時ごろをお知らせします。おやつです。'
+        post_str = f'{here_txt}{hour} 時ごろをお知らせします。おやつです。'
     else:
         if debug:
             hourminute = bu.get_now_datetime_str('HM')
-            post_str = f'@here {hourminute} ごろをお知らせします。'
+            post_str = f'{here_txt}{hourminute} ごろをお知らせします。'
             print(post_str)
         else:
-            post_str = f'@here {hour} 時ごろをお知らせします。'
+            post_str = f'{here_txt}{hour} 時ごろをお知らせします。'
     
     su.discord_post(CHANNEL, f'{post_str}{debug_str}')
     if hour == 2 or debug_arg == 2:
