fc2a3e3e0e42ad0dca4aff5c9c03f6261919b3f3
swirhen <irankae1#>
2021-05-31 14:20:26 +0900

    debug

--- python-lib/make_feed_list.py
+++ python-lib/make_feed_list.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # make feed list
 import pathlib
@@ -10,9 +9,10 @@ SCRIPT_DIR = str(current_dir)
 FILENAME = f'{SCRIPT_DIR}/../../98 PSP用/swirhentv_feed_list.txt'
 
 
-result = swiutil.get_feed_xml_list()
-result_str = ''
-for item in result:
-    if not re.search('ラジオ', item[1]):
-        result_str += f'{item[0]}: {item[1]}\n'
-swiutil.writefile_new(FILENAME, result_str)
+if __name__ == "__main__":
+    result = swiutil.get_feed_xml_list()
+    result_str = ''
+    for item in result:
+        if not re.search('ラジオ', item[1]):
+            result_str += f'{item[0]}: {item[1]}\n'
+    swiutil.writefile_new(FILENAME, result_str)
