eded27ae1974f2718f925ca49e2a781235a97159
swirhen <irankae1#>
2021-06-02 10:25:30 +0900

    date

--- python-lib/make_feed_db.py
+++ python-lib/make_feed_db.py
@@ -23,12 +23,13 @@ def make_feed_list_data():
     create_table_sql = 'create table if not exists feed(' \
                         ' name string primary key,' \
                         ' title string,' \
-                        ' path string)'
+                        ' path string,' \
+                        ' created_on string)'
     cur.execute(drop_table_sql)
     cur.execute(create_table_sql)
     values = []
     for xml_info in xml_infos:
-        values.append(f'("{xml_info[0]}", "{xml_info[1]}", "{xml_info[2]}")')
+        values.append(f'("{xml_info[0]}", "{xml_info[1]}", "{xml_info[2]}", "localtime")')
     values_str = ', '.join(values)
     insert_sql = f'insert into feed values{values_str}'
     cur.execute(insert_sql)
@@ -43,7 +44,8 @@ def make_feed_data(argument=''):
     drop_table_sql = 'drop table if exists feed_data'
     create_table_sql = 'create table if not exists feed_data(' \
                         ' name string,' \
-                        ' title string)'
+                        ' title string,' \
+                        ' created_on string)'
     delete_record_sql = 'delete from feed_data where name'
     if argument != '':
         xml_file = f'"{FEED_XML_DIR}/{argument}.xml"'
@@ -57,7 +59,7 @@ def make_feed_data(argument=''):
     values = []
     for feed in feeds:
         feed_info = feed.split('|')
-        values.append(f'("{feed_info[0]}", "{feed_info[1]}")')
+        values.append(f'("{feed_info[0]}", "{feed_info[1]}", "localtime")')
     values_str = ', '.join(values)
     insert_sql = f'insert into feed_data values{values_str}'
     cur.execute(insert_sql)
