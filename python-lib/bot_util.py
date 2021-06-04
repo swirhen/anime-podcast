bc3b97a1b2c9be018aac3d969e5e0872d271ab9f
swirhen <irankae1#>
2021-06-04 13:11:57 +0900

    debug

--- python-lib/bot_util.py
+++ python-lib/bot_util.py
@@ -401,11 +401,11 @@ def mhrize_weapon_expected_value_calc(melee, attribute, sharpness, crit):
         if crit != '':
             crit_str = f' / 会心: {crit}%'
             crit_str2 = f' * 会心補正(1 + (0.25 * {int(crit) / 100}))'
-        post_str = f'[MHRize weapon expected value calculator] (｀・ω・´)\n攻撃力: {melee} / 斬れ味: {sharpness}{attr_str}{crit_str}\nの武器の期待値は～\n'
+        post_str = f'[MHRize weapon expected value calculator]\n攻撃力: {melee} / 斬れ味: {sharpness}{attr_str}{crit_str}\nの武器の期待値は～\n'
         post_str += f'物理: {round(int(melee) * melee_mag * (1 + (0.25 * (int(crit) / 100))))} ({melee} * 斬れ味補正({melee_mag}){crit_str2})\n'
         if attribute != '':
             post_str += f'属性: {round(int(attribute) * attribute_mag)} ({attribute} * 斬れ味補正({attribute_mag})\n'
-        post_str += f'合計 {round(int(melee) * melee_mag * (1 + (0.25 * (int(crit) / 100)))) + round(int(attribute) * attribute_mag)} なのら～'
+        post_str += f'合計 {round(int(melee) * melee_mag * (1 + (0.25 * (int(crit) / 100)))) + round(int(attribute) * attribute_mag)} なのら～(｀・ω・´)'
 
         return post_str
 
