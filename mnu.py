7966afab2b0928d24ddea005454e758aa40395e3
swirhen <irankae1#>
2021-07-06 12:03:09 +0900

    debug

--- mnu.py
+++ mnu.py
@@ -11,32 +11,40 @@ import os
 import re
 import sys
 
-# main section
-args = sys.argv
-ARG = ''
-if len(args) == 2:
-    ARG = args[1]
 
-dirlist = glob.glob('*/')
-dirlist.sort()
+def mnu(arg=''):
+    dirlist = glob.glob('*/')
+    dirlist.sort()
 
-num = 1
-for directory in dirlist:
-    exp = r'^00|^9[2-9]|^sh'
-    if re.match(exp, directory):
-        print(f'{directory} : 処理除外')
-        continue
-    else:
-        exp = r'^[0-9][0-9]\ (.*)'
-        newdirname = re.sub(exp, r'\1', directory)
-        os.rename(directory, newdirname)
-        if ARG == '-r':
-            if directory != newdirname:
-                print(f'# rename {directory} -> {newdirname}')
+    num = 1
+    for directory in dirlist:
+        exp = r'^00|^9[2-9]|^sh'
+        if re.match(exp, directory):
+            print(f'{directory} : 処理除外')
             continue
         else:
-            numstr = '{0:02d}'.format(num)
-            os.rename(newdirname, numstr + ' ' + newdirname)
-            if directory != numstr + ' ' + newdirname:
-                print(f'# rename {directory} -> {numstr} {newdirname}')
-            num += 1
+            exp = r'^[0-9][0-9]\ (.*)'
+            newdirname = re.sub(exp, r'\1', directory)
+            os.rename(directory, newdirname)
+            if arg == '-r':
+                if directory != newdirname:
+                    print(f'# rename {directory} -> {newdirname}')
+                continue
+            else:
+                numstr = '{0:02d}'.format(num)
+                os.rename(newdirname, numstr + ' ' + newdirname)
+                if directory != numstr + ' ' + newdirname:
+                    print(f'# rename {directory} -> {numstr} {newdirname}')
+                num += 1
+
+
+# main section
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) == 2:
+        mnu(args[1])
+    elif len(args) == 1:
+        mnu()
+    else:
+        print(f'usage: {args[0]} (-r)')
+        exit(1)
