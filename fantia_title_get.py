c71d1db2389a7a6141e122c59b563b0b10865b87
Junichi Suzuki <junichi.suzuki@nxtg.co.jp>
2025-06-09 14:49:46 +0900

    debug

--- fantia_title_get.py
+++ fantia_title_get.py
@@ -8,6 +8,15 @@ from bs4 import BeautifulSoup
 # main section
 if __name__ == '__main__':
     args = sys.argv
-    html = urllib.request.urlopen(args[1])
+    header = args[2]
+    html = urllib.request.urlopen(args[1])    
     soup = BeautifulSoup(html, "html.parser")
-    print(re.sub(r'\<.*?\>|\ -.*|【.*?】|\ ','', str(soup.find('title'))))
+    title = re.sub(r'\<.*?\>|\ -.*|【.*?】|\ ','', str(soup.find('title')))
+    rtitle = header + title
+    if len(rtitle.encode('utf-8')) > 255:
+        over = len(rtitle.encode('utf-8')) - 255
+        cut = len(title.encode('utf-8')) - over
+        ret = title.encode('utf-8')[0:cut].decode('utf-8', errors='ignore')
+    else:
+        ret = title
+print(ret)
