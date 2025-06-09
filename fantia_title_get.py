2c0b1157c664bc61db3fffeb7840de1788e2016f
Junichi Suzuki <junichi.suzuki@nxtg.co.jp>
2025-06-09 13:48:48 +0900

    hoge

---
+++ fantia_title_get.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# import section
+import sys,re
+import urllib.request
+from bs4 import BeautifulSoup
+
+# main section
+if __name__ == '__main__':
+    args = sys.argv
+    html = urllib.request.urlopen(f'args[0]')
+    soup = BeautifulSoup(html, "html.parser")
+    print(re.sub(r'\<.*?\>|\ -.*|【.*】|\ ','', str(soup.find('title'))))
