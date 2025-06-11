1868640218ace1e7cfb320dfbeb56e5bed714a25
swirhen <swirhen@gmail.com>
2025-06-11 11:15:55 +0900

    debug

--- python-lib/swirhentv_util.py
+++ python-lib/swirhentv_util.py
@@ -96,19 +96,19 @@ def discord_upload(channel, filename):
 
 # discord/slack multi post
 def multi_post(channel, text, username='swirhentv', icon_emoji=''):
-    if len(text) > 4000:
-        if text.split('\n')[0][0] == '@':
-            slack_post(channel, text.split('\n')[0])
-            text = re.sub('^.*\n', '', text)
-        if re.search('```', text):
-            text = text.replace('```', '')
-        date_time = dt.now().strftime('%Y%m%d%H%M%S')
-        post_file_temp = f'{SCRIPT_DIR}/slack_post_temp_{date_time}.txt'
-        writefile_new(post_file_temp, text)
-        slack_upload(channel, post_file_temp)
-        os.remove(post_file_temp)
-    else:
-        slack_post(channel, text, username, icon_emoji)
+#    if len(text) > 4000:
+#        if text.split('\n')[0][0] == '@':
+#            slack_post(channel, text.split('\n')[0])
+#            text = re.sub('^.*\n', '', text)
+#        if re.search('```', text):
+#            text = text.replace('```', '')
+#        date_time = dt.now().strftime('%Y%m%d%H%M%S')
+#        post_file_temp = f'{SCRIPT_DIR}/slack_post_temp_{date_time}.txt'
+#        writefile_new(post_file_temp, text)
+#        slack_upload(channel, post_file_temp)
+#        os.remove(post_file_temp)
+#    else:
+#        slack_post(channel, text, username, icon_emoji)
 
     if len(text) > 2000:
         if text.split('\n')[0][0] == '@':
@@ -127,7 +127,7 @@ def multi_post(channel, text, username='swirhentv', icon_emoji=''):
 
 # discord/slack multi upload
 def multi_upload(channel, filename, filetype='text'):
-    slack_upload(channel, filename, filetype)
+#    slack_upload(channel, filename, filetype)
     discord_upload(channel, filename)
 
 
