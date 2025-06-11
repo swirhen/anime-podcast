6467ba92f6d0426e55d2e180fc3266c1f2a13d64
= <=>
2025-06-11 11:53:49 +0900

    conbine

---
+++ title_get.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# import section
+import sys,re,pathlib
+import urllib.request
+from bs4 import BeautifulSoup
+current_dir = pathlib.Path(__file__).resolve().parent
+sys.path.append(f'{str(current_dir)}/python-lib/')
+import swirhentv_util as swiutil
+
+
+def get_fantia_title(name, regexp='\<.*?\>|\ -.*|【.*?】', uri='https://fantia.jp/posts/'):
+    id = re.sub(r'\D', '', name)
+    html = urllib.request.urlopen(uri + id)
+    soup = BeautifulSoup(html, "html.parser")
+    title = re.sub(regexp, '', str(soup.find('title'))).strip()
+    rtitle = name + " - " + title
+    ret = swiutil.file_name_cut(rtitle)
+    return ret
+
+
+# main section
+if __name__ == '__main__':
+    args = sys.argv
+    mode = args[1]
+    keyword = args[2]
+    regexp = ''
+    uri = ''
+    if len(args) > 3:
+        regexp = args[3]
+    if len(args) > 4:
+        uri = args[4]
+    
+    if mode == 'f':
+        ret = get_fantia_title(keyword, regexp, uri)
+
+print(ret)
