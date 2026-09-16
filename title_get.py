50e2d035696c613396058e02f27a3b0adc982032
swirhen <swirhen@gmail.com>
2026-09-17 00:36:00 +0900

    debug

--- title_get.py
+++ title_get.py
@@ -9,6 +9,24 @@ sys.path.append(f'{str(current_dir)}/python-lib/')
 import swirhentv_util as swiutil
 import torrent_search_common as ts
 
+WINDOWS_RESERVED_FILENAMES = {
+    'CON', 'PRN', 'AUX', 'NUL',
+    'COM1', 'COM2', 'COM3', 'COM4', 'COM5', 'COM6', 'COM7', 'COM8', 'COM9',
+    'LPT1', 'LPT2', 'LPT3', 'LPT4', 'LPT5', 'LPT6', 'LPT7', 'LPT8', 'LPT9'
+}
+
+
+# フォルダ名の安全化(Windows禁止文字・制御文字を置換)
+# 末尾の空白・ドットはWindowsで使用できないため除去
+# ファイル名上限255 bytesに制限
+def sanitize_filename(title, max_bytes=255):
+    filename = re.sub(r'[<>:"/\\|?*\x00-\x1f]', '_', title).rstrip(' .')
+    if not filename:
+        filename = '_'
+    if filename.split('.')[0].upper() in WINDOWS_RESERVED_FILENAMES:
+        filename = f'_{filename}'
+    return swiutil.truncate(filename, max_bytes)
+
 
 # fantiaのタイトル取得(入れたキーワードは頭につけて「 - 」で連結して返す)
 def get_fantia_title(keyword, regexp='\<.*?\>|\ -.*|【.*?】', uri='https://fantia.jp/posts/'):
@@ -22,7 +40,7 @@ def get_fantia_title(keyword, regexp='\<.*?\>|\ -.*|【.*?】', uri='https://fan
     soup = BeautifulSoup(html, "html.parser")
     title = re.sub(regexp, '', str(soup.find('title'))).strip()
     rtitle = keyword + " - " + title
-    ret = swiutil.file_name_cut(rtitle)
+    ret = sanitize_filename(rtitle)
     return ret
 
 
@@ -36,7 +54,7 @@ def get_av_title(keyword, regexp='\+\+\+|\[.*?\]', cut=True):
         title = result[0][1]
         title = re.sub(regexp, '', title).strip()
         if cut:
-            ret = swiutil.file_name_cut(title)
+            ret = sanitize_filename(title)
         else:
             if len(title.encode('utf-8')) > 255:
                 title = title + '### ' + str(len(title.encode('utf-8'))) + 'bytes'
